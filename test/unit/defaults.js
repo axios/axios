e1aa04cceeeb72e52b41c891d1fd0325db96a0f7
mzabriskie <mzabriskie@gmail.com>
2014-09-16 18:23:31 -0600

    Fixing issue #3

---
+++ test/unit/defaults.js
@@ -0,0 +1,32 @@
+var defaults = require('../../lib/defaults');
+
+module.exports = {
+  testTransformRequestJson: function (test) {
+    var data = defaults.transformRequest[0]({foo: 'bar'});
+
+    test.equals(data, '{"foo":"bar"}');
+    test.done();
+  },
+
+  testTransformRequestString: function (test) {
+    var data = defaults.transformRequest[0]('foo=bar');
+
+    test.equals(data, 'foo=bar');
+    test.done();
+  },
+
+  testTransformResponseJson: function (test) {
+    var data = defaults.transformResponse[0]('{"foo":"bar"}');
+
+    test.equals(typeof data, 'object');
+    test.equals(data.foo, 'bar');
+    test.done();
+  },
+
+  testTransformResponseString: function (test) {
+    var data = defaults.transformResponse[0]('foo=bar');
+
+    test.equals(data, 'foo=bar');
+    test.done();
+  }
+};

a9bcc8f4190a6d37a14a2d9d6d479c063994d9bb
mzabriskie <mzabriskie@gmail.com>
2015-01-23 02:53:22 -0700

    Upgrading es6-promise dependency

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'dist/axios.js',
-      'node_modules/es6-promise/dist/promise-1.0.0.js',
       'test/specs/**/*.spec.js'
     ],
 

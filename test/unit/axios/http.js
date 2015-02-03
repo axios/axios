0412c7666e72f17e5359a1106f20b79e59f1b3c1
mzabriskie <mzabriskie@gmail.com>
2015-02-02 22:04:18 -0700

    Fixing issue with multibyte characters in node closes #38

---
+++ test/unit/axios/http.js
@@ -0,0 +1,44 @@
+var axios = require('../../../index');
+var http = require('http');
+var server;
+
+module.exports = {
+  tearDown: function (callback) {
+    server.close();
+    server = null;
+    callback();
+  },
+
+  testJSON: function (test) {
+    console.log('testJSON');
+    var data = {
+      firstName: 'Fred',
+      lastName: 'Flintstone',
+      emailAddr: 'fred@example.com'
+    };
+
+    server = http.createServer(function (req, res) {
+      res.setHeader('Content-Type', 'application/json;charset=utf-8');
+      res.end(JSON.stringify(data));
+    }).listen(4444, function () {
+      axios.get('http://localhost:4444/').then(function (res) {
+        test.deepEqual(res.data, data);
+        test.done();
+      });
+    });
+  },
+
+  testUTF8: function (test) {
+    var str = Array(100000).join('ж');
+
+    server = http.createServer(function (req, res) {
+      res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+      res.end(str);
+    }).listen(4444, function () {
+      axios.get('http://localhost:4444/').then(function (res) {
+        test.equal(res.data, str);
+        test.done();
+      });
+    });
+  }
+};

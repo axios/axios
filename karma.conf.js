ce03103da4fb30642a5162a69daf08ef7b69a060
mzabriskie <mzabriskie@gmail.com>
2015-03-17 16:56:30 -0600

    Adding coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -29,15 +29,20 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-
+      'dist/axios.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/',
+      subdir: '.'
+    },
 
     // web server port
     port: 9876,

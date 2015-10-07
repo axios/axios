fcd2121c8b7eb1338c44af4b4ecc16af4e32869f
mzabriskie <mzabriskie@gmail.com>
2015-10-07 14:36:35 -0600

    Fixing test coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'webpack.tests.js'
+      'test/specs/**/*.spec.js'
     ],
 
 
@@ -28,17 +28,27 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'webpack.tests.js': ['webpack', 'sourcemap', 'coverage']
+      'test/specs/**/*.spec.js': ['webpack', 'sourcemap']
     },
 
 
     webpack: {
+      cache: true,
+      devtool: 'inline-source-map',
+      module: {
+        postLoaders: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|test)/,
+            loader: 'istanbul-instrumenter'
+          }
+        ]
+      },
       externals: [
         {
           './adapters/http': 'var undefined'
         }
-      ],
-      devtool: 'inline-source-map'
+      ]
     },
 
     webpackServer: {

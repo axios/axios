7a16f72895754fda7e80635d5c160357f4a7aaed
Matt Zabriskie <mzabriskie@gmail.com>
2016-01-22 16:27:54 -0700

    Improving logic for handling timeout in node closes #124

--- test/unit/adapters/http.js
+++ test/unit/adapters/http.js
@@ -11,6 +11,34 @@ module.exports = {
     callback();
   },
 
+  testTimeout: function (test) {
+    server = http.createServer(function (req, res) {
+      setTimeout(function () {
+        res.end();
+      }, 1000);
+    }).listen(4444, function () {
+      var success = false, failure = false;
+      var error;
+
+      axios.get('http://localhost:4444/', {
+        timeout: 250
+      }).then(function (res) {
+        success = true;
+      }).catch(function (res) {
+        error = res;
+        failure = true;
+      });
+
+      setTimeout(function () {
+        test.equal(success, false, 'request should not succeed');
+        test.equal(failure, true, 'request should fail');
+        test.equal(error.code, 'ECONNABORTED');
+        test.equal(error.message, 'timeout of 250ms exceeded');
+        test.done();
+      }, 300);
+    });
+  },
+
   testJSON: function (test) {
     var data = {
       firstName: 'Fred',

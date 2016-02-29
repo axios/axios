116f5fca886b5fd9733c1d1548c5f76e220efb21
Andrew Crites <ajcrites@gmail.com>
2016-02-28 22:51:20 -0500

    Fixing 245:
    Formatting and adding tests

--- test/unit/adapters/http.js
+++ test/unit/adapters/http.js
@@ -113,5 +113,19 @@ module.exports = {
         test.done();
       });
     });
-  }
+  },
+
+  testBasicAuth: function (test) {
+    server = http.createServer(function (req, res) {
+      console.log(req.headers);
+      res.end(req.headers.authorization);
+    }).listen(4444, function () {
+      var user = 'foo';
+      axios.get('http://' + user + '@localhost:4444/').then(function (res) {
+        var base64 = new Buffer(user + ':', 'utf8').toString('base64');
+        test.equal(res.data, 'Basic ' + base64);
+        test.done();
+      });
+    });
+  },
 };

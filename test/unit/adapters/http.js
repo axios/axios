5c5a94007c3c1807e0fd3c99457e1603b0d02511
Nick Uraltsev <nick.uraltsev@gmail.com>
2016-05-31 18:29:32 -0700

    Adding test for maxRedirects option

--- test/unit/adapters/http.js
+++ test/unit/adapters/http.js
@@ -98,6 +98,22 @@ module.exports = {
     });
   },
 
+  testMaxRedirects: function (test) {
+    var i = 1;
+    server = http.createServer(function (req, res) {
+      res.setHeader('Location', '/' + i);
+      res.statusCode = 302;
+      res.end();
+      i++;
+    }).listen(4444, function () {
+      axios.get('http://localhost:4444/', {
+        maxRedirects: 3
+      }).catch(function (error) {
+        test.done();
+      });
+    });
+  },
+
   testTransparentGunzip: function (test) {
     var data = {
       firstName: 'Fred',

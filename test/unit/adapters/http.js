feec1fc22cc67c9570753620a184e490fd2acf2e
bomsy <b4bomsy@gmail.com>
2016-07-05 12:32:54 +0100

    support process.env.HTTP(HTTPS)_PROXY, process.env.http(https)_proxy

--- test/unit/adapters/http.js
+++ test/unit/adapters/http.js
@@ -251,10 +251,9 @@ module.exports = {
   testProxy: function(test) {
     server = http.createServer(function(req, res) {
       res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-      res.end('server response');
+      res.end('12345');
     }).listen(4444, function() {
-      // proxy
-      var proxyServer = http.createServer(function(request, response) {
+      proxy = http.createServer(function(request, response) {
         var parsed = url.parse(request.url);
         var opts = {
           host: parsed.hostname,
@@ -269,7 +268,7 @@ module.exports = {
           });
           res.on('end', function() {
             response.setHeader('Content-Type', 'text/html; charset=UTF-8');
-            response.end(body + ' through proxy');
+            response.end(body + '6789');
           });
         });
 
@@ -316,7 +315,7 @@ module.exports = {
         process.env.http_proxy = 'http://localhost:4000/';
 
         axios.get('http://localhost:4444/').then(function(res) {
-          test.equal(res.data, 'server response through proxy');
+          test.equal(res.data, '45671234', 'should use proxy set by process.env.http_proxy');
           test.done();
         });
       });

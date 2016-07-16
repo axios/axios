0578445929019efe370dae56cd6f39647e8a73c1
Matt Zabriskie <mzabriskie@gmail.com>
2016-07-16 11:15:46 -0600

    Moar tests

--- test/specs/requests.spec.js
+++ test/specs/requests.spec.js
@@ -138,8 +138,8 @@ describe('requests', function () {
 
       setTimeout(function () {
         expect(typeof response.data).toEqual('object');
-        // expect(response.data.error).toEqual('BAD USERNAME');
-        // expect(response.data.code).toEqual(1);
+        expect(response.data.error).toEqual('BAD USERNAME');
+        expect(response.data.code).toEqual(1);
         done();
       });
     });

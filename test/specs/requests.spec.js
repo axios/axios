98d489558e6ac0829d9eb8e6ca3a4d6800fd7a7a
Matt Zabriskie <mzabriskie@gmail.com>
2016-07-16 11:10:58 -0600

    Transform response data on error closes #378

--- test/specs/requests.spec.js
+++ test/specs/requests.spec.js
@@ -112,6 +112,39 @@ describe('requests', function () {
     });
   });
 
+  // https://github.com/mzabriskie/axios/issues/378
+  it('should return JSON when rejecting', function (done) {
+    var response;
+
+    axios('/api/account/signup', {
+      username: null,
+      password: null
+    }, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .catch(function (error) {
+      response = error.response;
+    });
+
+    getAjaxRequest().then(function (request) {
+      request.respondWith({
+        status: 400,
+        statusText: 'Bad Request',
+        responseText: '{"error": "BAD USERNAME", "code": 1}'
+      });
+
+      setTimeout(function () {
+        expect(typeof response.data).toEqual('object');
+        // expect(response.data.error).toEqual('BAD USERNAME');
+        // expect(response.data.code).toEqual(1);
+        done();
+      });
+    });
+  });
+
   it('should make cross domian http request', function (done) {
     var response;
 

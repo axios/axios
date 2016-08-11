e861a6cf756d25598c32ee8531a4d8c7e54fbb8d
Marco Pracucci <marco.pracucci@spreaker.com>
2016-08-11 10:47:53 +0200

    Added test to ensure that XHR adapter does not read cookies if xsrfCookieName is null

--- test/specs/xsrf.spec.js
+++ test/specs/xsrf.spec.js
@@ -1,3 +1,5 @@
+var cookies = require('../../lib/helpers/cookies');
+
 describe('xsrf', function () {
   beforeEach(function () {
     jasmine.Ajax.install();
@@ -37,6 +39,19 @@ describe('xsrf', function () {
 
     getAjaxRequest().then(function (request) {
       expect(request.requestHeaders[axios.defaults.xsrfHeaderName]).toEqual(undefined);
+      done();
+    });
+  });
+
+  it('should not read cookies at all if xsrfCookieName is null', function (done) {
+    spyOn(cookies, "read");
+
+    axios('/foo', {
+      xsrfCookieName: null
+    });
+
+    getAjaxRequest().then(function (request) {
+      expect(cookies.read).not.toHaveBeenCalled();
       done();
     });
   });

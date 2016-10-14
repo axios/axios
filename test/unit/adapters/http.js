853740fdcbe6ad47aad3a31236fd95da13562d77
mawrkus <web@sparring-partner.be>
2016-10-14 15:00:31 +0200

    Adding 3 new unit tests for proxy auth

--- test/unit/adapters/http.js
+++ test/unit/adapters/http.js
@@ -322,6 +322,125 @@ module.exports = {
     });
   },
 
+  testHTTPProxyAuth: function(test) {
+    server = http.createServer(function(req, res) {
+      res.end();
+    }).listen(4444, function() {
+      proxy = http.createServer(function(request, response) {
+        var parsed = url.parse(request.url);
+        var opts = {
+          host: parsed.hostname,
+          port: parsed.port,
+          path: parsed.path
+        };
+        var proxyAuth = request.headers['proxy-authorization'];
+
+        http.get(opts, function(res) {
+          var body = '';
+          res.on('data', function(data) {
+            body += data;
+          });
+          res.on('end', function() {
+            response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+            response.end(proxyAuth);
+          });
+        });
+
+      }).listen(4000, function() {
+        axios.get('http://localhost:4444/', {
+          proxy: {
+            host: 'localhost',
+            port: 4000,
+            auth: 'user:pass'
+          }
+        }).then(function(res) {
+          var base64 = new Buffer('user:pass', 'utf8').toString('base64');
+          test.equal(res.data, 'Basic ' + base64, 'should authenticate to the proxy');
+          test.done();
+        });
+      });
+    });
+  },
+
+  testHTTPProxyAuthFromEnv: function(test) {
+    server = http.createServer(function(req, res) {
+      res.end();
+    }).listen(4444, function() {
+      proxy = http.createServer(function(request, response) {
+        var parsed = url.parse(request.url);
+        var opts = {
+          host: parsed.hostname,
+          port: parsed.port,
+          path: parsed.path
+        };
+        var proxyAuth = request.headers['proxy-authorization'];
+
+        http.get(opts, function(res) {
+          var body = '';
+          res.on('data', function(data) {
+            body += data;
+          });
+          res.on('end', function() {
+            response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+            response.end(proxyAuth);
+          });
+        });
+
+      }).listen(4000, function() {
+        process.env.http_proxy = 'http://user:pass@localhost:4000/';
+
+        axios.get('http://localhost:4444/').then(function(res) {
+          var base64 = new Buffer('user:pass', 'utf8').toString('base64');
+          test.equal(res.data, 'Basic ' + base64, 'should authenticate to the proxy set by process.env.http_proxy');
+          test.done();
+        });
+      });
+    });
+  },
+
+  testHTTPProxyAuthWithHeader: function (test) {
+    server = http.createServer(function(req, res) {
+      res.end();
+    }).listen(4444, function() {
+      proxy = http.createServer(function(request, response) {
+        var parsed = url.parse(request.url);
+        var opts = {
+          host: parsed.hostname,
+          port: parsed.port,
+          path: parsed.path
+        };
+        var proxyAuth = request.headers['proxy-authorization'];
+
+        http.get(opts, function(res) {
+          var body = '';
+          res.on('data', function(data) {
+            body += data;
+          });
+          res.on('end', function() {
+            response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+            response.end(proxyAuth);
+          });
+        });
+
+      }).listen(4000, function() {
+        axios.get('http://localhost:4444/', {
+          proxy: {
+            host: 'localhost',
+            port: 4000,
+            auth: 'user:pass'
+          },
+          headers: {
+            'Proxy-Authorization': 'Basic abc123'
+          }
+        }).then(function(res) {
+          var base64 = new Buffer('user:pass', 'utf8').toString('base64');
+          test.equal(res.data, 'Basic ' + base64, 'should authenticate to the proxy');
+          test.done();
+        });
+      });
+    });
+  },
+
   testCancel: function(test) {
     var source = axios.CancelToken.source();
     server = http.createServer(function (req, res) {

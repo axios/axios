fe7d09bb08fa1c0e414956b7fc760c80459b0a43
Lochlan Bunn <bunn@lochlan.io>
2016-12-08 15:23:45 +1000

    Fixing combineURLs to support an empty relativeURL (#581)
    * Fixing combineURLs to support an empty relativeURL
    
    When combining the base and relative URLs, we should forego force
    appending a slash to the base when the relative URL is empty.
    This leads to a semantic url.
    
    * Fixing combineURLs, allowing single slash relatives

--- test/specs/helpers/combineURLs.spec.js
+++ test/specs/helpers/combineURLs.spec.js
@@ -12,4 +12,12 @@ describe('helpers::combineURLs', function () {
   it('should insert missing slash', function () {
     expect(combineURLs('https://api.github.com', 'users')).toBe('https://api.github.com/users');
   });
+
+  it('should not insert slash when relative url missing/empty', function () {
+    expect(combineURLs('https://api.github.com/users', '')).toBe('https://api.github.com/users');
+  });
+
+  it('should allow a single slash for relative url', function () {
+    expect(combineURLs('https://api.github.com/users', '/')).toBe('https://api.github.com/users/');
+  });
 });

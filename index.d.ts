e7c3d3fc82b97a5bdecdd185a56ffc5618698dd9
Martin Tange <mktange@gmail.com>
2016-12-12 10:40:30 +0100

    Removed custom typings for the Promise-interface
    It differs from the standard Promise-interface that comes with TypeScript, making it incompatible with other uses of promises.
    
    If targeting ES5 or lower, then you can add "es2015.promise" to the "lib" option of your tsconfig.json file to include support for ES2015 promises.

--- index.d.ts
+++ index.d.ts
@@ -58,12 +58,6 @@ export interface AxiosError extends Error {
   response?: AxiosResponse;
 }
 
-export interface Promise<V> {
-  then<R1, R2>(onFulfilled: (value: V) => R1 | Promise<R1>, onRejected: (error: any) => R2 | Promise<R2>): Promise<R1 | R2>;
-  then<R>(onFulfilled: (value: V) => R | Promise<R>): Promise<R>;
-  catch<R>(onRejected: (error: any) => R | Promise<R>): Promise<R>;
-}
-
 export interface AxiosPromise extends Promise<AxiosResponse> {
 }
 

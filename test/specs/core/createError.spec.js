e0d59eb29bba887cb4376d1c37614dd1c2741327
Rubén Norte <rubennorte@gmail.com>
2017-04-08 21:43:04 +0200

    Adding failing tests that verify errors contain the request

--- test/specs/core/createError.spec.js
+++ test/specs/core/createError.spec.js
@@ -1,11 +1,15 @@
 var createError = require('../../../lib/core/createError');
 
 describe('core::createError', function() {
-  it('should create an Error with message, config, and code', function() {
-    var error = createError('Boom!', { foo: 'bar' }, 'ESOMETHING');
+  it('should create an Error with message, config, code, request and response', function() {
+    var request = { path: '/foo' };
+    var response = { status: 200, data: { foo: 'bar' } };
+    var error = createError('Boom!', { foo: 'bar' }, 'ESOMETHING', request, response);
     expect(error instanceof Error).toBe(true);
     expect(error.message).toBe('Boom!');
     expect(error.config).toEqual({ foo: 'bar' });
     expect(error.code).toBe('ESOMETHING');
+    expect(error.request).toBe(request);
+    expect(error.response).toBe(response);
   });
 });

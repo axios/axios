4007cd9f6bb242df8e9657d3d8f2eb53b3cca6aa
Leonid Nikiforenko <leo.nikiforenko@gmail.com>
2017-08-13 00:35:14 +0300

    Preserve order of request interceptors Fixes #841

--- test/specs/interceptors.spec.js
+++ test/specs/interceptors.spec.js
@@ -66,24 +66,22 @@ describe('interceptors', function () {
 
   it('should add multiple request interceptors', function (done) {
     axios.interceptors.request.use(function (config) {
-      config.headers.test1 = '1';
+      config.headers.test = '1';
       return config;
     });
     axios.interceptors.request.use(function (config) {
-      config.headers.test2 = '2';
+      config.headers.test += '2';
       return config;
     });
     axios.interceptors.request.use(function (config) {
-      config.headers.test3 = '3';
+      config.headers.test += '3';
       return config;
     });
 
     axios('/foo');
 
     getAjaxRequest().then(function (request) {
-      expect(request.requestHeaders.test1).toBe('1');
-      expect(request.requestHeaders.test2).toBe('2');
-      expect(request.requestHeaders.test3).toBe('3');
+      expect(request.requestHeaders.test).toBe('123');
       done();
     });
   });

6e58fa94d118dd8ea2433ae67d9deaa4ead1cfbe
38elements <mh19820223@gmail.com>
2017-11-18 18:17:42 +0900

    Adding test for non standard browser envs

--- test/specs/helpers/cookies.spec.js
+++ test/specs/helpers/cookies.spec.js
@@ -1,6 +1,5 @@
-var cookies = require('../../../lib/helpers/cookies');
-
 describe('helpers::cookies', function () {
+
   afterEach(function () {
     // Remove all the cookies
     var expires = Date.now() - (60 * 60 * 24 * 7);
@@ -11,26 +10,66 @@ describe('helpers::cookies', function () {
     });
   });
 
-  it('should write cookies', function () {
-    cookies.write('foo', 'baz');
-    expect(document.cookie).toEqual('foo=baz');
+  describe('standard browser envs', function () {
+    var cookies;
+
+    beforeAll(function() {
+      delete require.cache[require.resolve('../../../lib/helpers/cookies')];
+      cookies = require('../../../lib/helpers/cookies');
+    });
+
+    it('should write cookies', function () {
+      cookies.write('foo', 'baz');
+      expect(document.cookie).toEqual('foo=baz');
+    });
+
+    it('should read cookies', function () {
+      cookies.write('foo', 'abc');
+      cookies.write('bar', 'def');
+      expect(cookies.read('foo')).toEqual('abc');
+      expect(cookies.read('bar')).toEqual('def');
+    });
+
+    it('should remove cookies', function () {
+      cookies.write('foo', 'bar');
+      cookies.remove('foo');
+      expect(cookies.read('foo')).toEqual(null);
+    });
+
+    it('should uri encode values', function () {
+      cookies.write('foo', 'bar baz%');
+      expect(document.cookie).toEqual('foo=bar%20baz%25');
+    });
   });
 
-  it('should read cookies', function () {
-    cookies.write('foo', 'abc');
-    cookies.write('bar', 'def');
-    expect(cookies.read('foo')).toEqual('abc');
-    expect(cookies.read('bar')).toEqual('def');
-  });
+  describe('non standard browser envs', function () {
+    var cookies;
 
-  it('should remove cookies', function () {
-    cookies.write('foo', 'bar');
-    cookies.remove('foo');
-    expect(cookies.read('foo')).toEqual(null);
-  });
+    beforeAll(function() {
+      delete require.cache[require.resolve('../../../lib/helpers/cookies')];
+      navigator.product = 'ReactNative';
+      cookies = require('../../../lib/helpers/cookies');
+    });
 
-  it('should uri encode values', function () {
-    cookies.write('foo', 'bar baz%');
-    expect(document.cookie).toEqual('foo=bar%20baz%25');
+    afterAll(function() {
+      navigator.product = undefined;
+    });
+
+    it('should not write cookies', function () {
+      cookies.write('foo', 'baz');
+      expect(document.cookie).toEqual('');
+    });
+
+    it('should always return null when read() is executed', function () {
+      cookies.write('foo', 'abc');
+      expect(cookies.read('foo')).toEqual(null);
+      expect(cookies.read('bar')).toEqual(null);
+    });
+
+    it('should do nothing when remove() is executed', function () {
+      document.cookie = 'foo=bar';
+      cookies.remove('foo');
+      expect(document.cookie).toEqual('foo=bar');
+    });
   });
 });

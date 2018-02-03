3489e0807c1edfc4ff1259e67bb784a614289171
Stephan Schneider <stephanschndr@gmail.com>
2018-02-03 21:22:18 +0100

    test: move specs/utils to ESM
    extend, forEach, merge and trim are replaced by lodash

--- test/specs/utils/isX.spec.js
+++ test/specs/utils/isX.spec.js
@@ -1,32 +1,21 @@
-const utils = require('../../../lib/utils')
-const Stream = require('stream')
+/* global isOldIE */
 
-describe('utils::isX', function () {
-  it('should validate Array', function () {
-    expect(utils.isArray([])).toEqual(true)
-    expect(utils.isArray({length: 5})).toEqual(false)
-  })
+import * as utils from '../../../lib/utils'
+import * as Stream from 'stream'
 
-  it('should validate ArrayBuffer', function () {
+describe('utils::isX', function a () {
+  it('should validate ArrayBufferView', function b () {
     // ArrayBuffer doesn't exist in IE8/9
     if (isOldIE && typeof ArrayBuffer === 'undefined') {
       return
     }
 
-    expect(utils.isArrayBuffer(new ArrayBuffer(2))).toEqual(true)
-    expect(utils.isArrayBuffer({})).toEqual(false)
-  })
-
-  it('should validate ArrayBufferView', function () {
-    // ArrayBuffer doesn't exist in IE8/9
-    if (isOldIE && typeof ArrayBuffer === 'undefined') {
-      return
-    }
+    console.log('dis is da utils', utils)
 
     expect(utils.isArrayBufferView(new DataView(new ArrayBuffer(2)))).toEqual(true)
   })
 
-  it('should validate FormData', function () {
+  it('should validate FormData', function c () {
     // FormData doesn't exist in IE8/9
     if (isOldIE && typeof FormData === 'undefined') {
       return
@@ -35,7 +24,7 @@ describe('utils::isX', function () {
     expect(utils.isFormData(new FormData())).toEqual(true)
   })
 
-  it('should validate Blob', function () {
+  it('should validate Blob', function d () {
     // Blob doesn't exist in IE8/9
     if (isOldIE && typeof Blob === 'undefined') {
       return
@@ -44,42 +33,12 @@ describe('utils::isX', function () {
     expect(utils.isBlob(new Blob())).toEqual(true)
   })
 
-  it('should validate String', function () {
-    expect(utils.isString('')).toEqual(true)
-    expect(utils.isString({toString: function () { return '' }})).toEqual(false)
-  })
-
-  it('should validate Number', function () {
-    expect(utils.isNumber(123)).toEqual(true)
-    expect(utils.isNumber('123')).toEqual(false)
-  })
-
-  it('should validate Undefined', function () {
-    expect(utils.isUndefined()).toEqual(true)
-    expect(utils.isUndefined(null)).toEqual(false)
-  })
-
-  it('should validate Object', function () {
-    expect(utils.isObject({})).toEqual(true)
-    expect(utils.isObject(null)).toEqual(false)
-  })
-
-  it('should validate Date', function () {
-    expect(utils.isDate(new Date())).toEqual(true)
-    expect(utils.isDate(Date.now())).toEqual(false)
-  })
-
-  it('should validate Function', function () {
-    expect(utils.isFunction(function () {})).toEqual(true)
-    expect(utils.isFunction('function')).toEqual(false)
-  })
-
-  it('should validate Stream', function () {
+  it('should validate Stream', function e () {
     expect(utils.isStream(new Stream.Readable())).toEqual(true)
     expect(utils.isStream({ foo: 'bar' })).toEqual(false)
   })
 
-  it('should validate URLSearchParams', function () {
+  it('should validate URLSearchParams', function f () {
     expect(utils.isURLSearchParams(new URLSearchParams())).toEqual(true)
     expect(utils.isURLSearchParams('foo=1&bar=2')).toEqual(false)
   })

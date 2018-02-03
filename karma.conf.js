b581b757e4a41bc92e1133f5b1b3313f07e02b53
Stephan Schneider <stephanschndr@gmail.com>
2018-02-03 19:57:35 +0100

    test(runner): change karma reporter

--- karma.conf.js
+++ karma.conf.js
@@ -136,7 +136,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'coverage', 'saucelabs'],
+    reporters: ['spec', 'coverage', 'saucelabs'],
 
     // web server port
     port: 9876,

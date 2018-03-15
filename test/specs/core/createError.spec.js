f32d0c2ec98ce288d1781d481335e70e8ebcbfa4
Ayush Gupta <AyushG3112@gmail.com>
2018-03-15 20:13:53 +0530

    Adding isAxiosError flag to errors thrown by axios

--- test/specs/core/createError.spec.js
+++ test/specs/core/createError.spec.js
@@ -1,7 +1,7 @@
 var createError = require('../../../lib/core/createError');
 
 describe('core::createError', function() {
-  it('should create an Error with message, config, code, request and response', function() {
+  it('should create an Error with message, config, code, request, response and isAxiosError', function() {
     var request = { path: '/foo' };
     var response = { status: 200, data: { foo: 'bar' } };
     var error = createError('Boom!', { foo: 'bar' }, 'ESOMETHING', request, response);
@@ -11,5 +11,6 @@ describe('core::createError', function() {
     expect(error.code).toBe('ESOMETHING');
     expect(error.request).toBe(request);
     expect(error.response).toBe(response);
+    expect(error.isAxiosError).toBe(true);
   });
 });

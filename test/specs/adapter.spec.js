05a196ba2af4b8e769dcca12433e555ea1a2bc73
George Duan <george.duan@linecorp.com>
2018-04-19 07:09:26 +0900

    Fix maxContentLength config not working for exceeded content-length response

--- test/specs/adapter.spec.js
+++ test/specs/adapter.spec.js
@@ -1,6 +1,14 @@
 var axios = require('../../index');
 
 describe('adapter', function () {
+  beforeEach(function () {
+    jasmine.Ajax.install();
+  });
+
+  afterEach(function () {
+    jasmine.Ajax.uninstall();
+  });
+
   it('should support custom adapter', function (done) {
     var called = false;
 
@@ -14,6 +22,28 @@ describe('adapter', function () {
       expect(called).toBe(true);
       done();
     }, 100);
+  });
+
+  it('should throw maxContentLength is exceed error', function(done) {
+    var testMaxContentLength = 10
+    axios.get('/foo', {maxContentLength: testMaxContentLength})
+      .then(function() {
+        fail('Ajax call should not success when the response is over maxContentLength.');
+        done();
+      })
+      .catch(function(err) {
+        expect(err.message).toBe(`maxContentLength size of ${testMaxContentLength} exceeded`);
+        done();
+      });
+    getAjaxRequest().then(function (request) {
+      request.respondWith({
+        status: 200,
+        responseText: 'OK',
+        responseHeaders: {
+          'Content-Length': testMaxContentLength + 1
+        }
+      });
+    });
   });
 });
 

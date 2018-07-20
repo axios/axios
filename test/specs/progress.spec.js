55598d51c09a8ca5c2122c8ca9cef3688a0efd48
kale-io <cos+a@iuvo.ai>
2018-07-20 10:51:51 -0700

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/specs/progress.spec.js
+++ test/specs/progress.spec.js
@@ -1,18 +1,18 @@
-describe('progress events', function () {
-  beforeEach(function () {
+describe('progress events', () => {
+  beforeEach(() => {
     jasmine.Ajax.install();
   });
 
-  afterEach(function () {
+  afterEach(() => {
     jasmine.Ajax.uninstall();
   });
 
-  it('should add a download progress handler', function (done) {
+  it('should add a download progress handler', done => {
     var progressSpy = jasmine.createSpy('progress');
 
     axios('/foo', { onDownloadProgress: progressSpy } );
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       request.respondWith({
         status: 200,
         responseText: '{"foo": "bar"}'
@@ -22,25 +22,25 @@ describe('progress events', function () {
     });
   });
 
-  it('should add a upload progress handler', function (done) {
+  it('should add a upload progress handler', done => {
     var progressSpy = jasmine.createSpy('progress');
 
     axios('/foo', { onUploadProgress: progressSpy } );
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       // Jasmine AJAX doesn't trigger upload events. Waiting for upstream fix
       // expect(progressSpy).toHaveBeenCalled();
       done();
     });
   });
 
-  it('should add both upload and download progress handlers', function (done) {
+  it('should add both upload and download progress handlers', done => {
     var downloadProgressSpy = jasmine.createSpy('downloadProgress');
     var uploadProgressSpy = jasmine.createSpy('uploadProgress');
 
     axios('/foo', { onDownloadProgress: downloadProgressSpy, onUploadProgress: uploadProgressSpy });
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       // expect(uploadProgressSpy).toHaveBeenCalled();
       expect(downloadProgressSpy).not.toHaveBeenCalled();
       request.respondWith({
@@ -52,7 +52,7 @@ describe('progress events', function () {
     });
   });
 
-  it('should add a download progress handler from instance config', function (done) {
+  it('should add a download progress handler from instance config', done => {
     var progressSpy = jasmine.createSpy('progress');
 
     var instance = axios.create({
@@ -61,7 +61,7 @@ describe('progress events', function () {
 
     instance.get('/foo');
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       request.respondWith({
         status: 200,
         responseText: '{"foo": "bar"}'
@@ -71,7 +71,7 @@ describe('progress events', function () {
     });
   });
 
-  it('should add a upload progress handler from instance config', function (done) {
+  it('should add a upload progress handler from instance config', done => {
     var progressSpy = jasmine.createSpy('progress');
 
     var instance = axios.create({
@@ -80,13 +80,13 @@ describe('progress events', function () {
 
     instance.get('/foo');
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       // expect(progressSpy).toHaveBeenCalled();
       done();
     });
   });
 
-  it('should add upload and download progress handlers from instance config', function (done) {
+  it('should add upload and download progress handlers from instance config', done => {
     var downloadProgressSpy = jasmine.createSpy('downloadProgress');
     var uploadProgressSpy = jasmine.createSpy('uploadProgress');
 
@@ -97,7 +97,7 @@ describe('progress events', function () {
 
     instance.get('/foo');
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       // expect(uploadProgressSpy).toHaveBeenCalled();
       expect(downloadProgressSpy).not.toHaveBeenCalled();
       request.respondWith({

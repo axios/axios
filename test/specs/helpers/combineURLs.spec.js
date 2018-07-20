6a96ec16d6738174f4c81e2487f91de69bbc18b5
kale-io <cos+a@iuvo.ai>
2018-07-20 10:50:16 -0700

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/specs/helpers/combineURLs.spec.js
+++ test/specs/helpers/combineURLs.spec.js
@@ -1,23 +1,23 @@
 var combineURLs = require('../../../lib/helpers/combineURLs');
 
-describe('helpers::combineURLs', function () {
-  it('should combine URLs', function () {
+describe('helpers::combineURLs', () => {
+  it('should combine URLs', () => {
     expect(combineURLs('https://api.github.com', '/users')).toBe('https://api.github.com/users');
   });
 
-  it('should remove duplicate slashes', function () {
+  it('should remove duplicate slashes', () => {
     expect(combineURLs('https://api.github.com/', '/users')).toBe('https://api.github.com/users');
   });
 
-  it('should insert missing slash', function () {
+  it('should insert missing slash', () => {
     expect(combineURLs('https://api.github.com', 'users')).toBe('https://api.github.com/users');
   });
 
-  it('should not insert slash when relative url missing/empty', function () {
+  it('should not insert slash when relative url missing/empty', () => {
     expect(combineURLs('https://api.github.com/users', '')).toBe('https://api.github.com/users');
   });
 
-  it('should allow a single slash for relative url', function () {
+  it('should allow a single slash for relative url', () => {
     expect(combineURLs('https://api.github.com/users', '/')).toBe('https://api.github.com/users/');
   });
 });

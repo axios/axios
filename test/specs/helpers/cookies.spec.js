ec597051534c79becff9164198b5a1e030f36626
kale-io <cos+a@iuvo.ai>
2018-07-20 10:50:21 -0700

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/specs/helpers/cookies.spec.js
+++ test/specs/helpers/cookies.spec.js
@@ -1,35 +1,33 @@
 var cookies = require('../../../lib/helpers/cookies');
 
-describe('helpers::cookies', function () {
-  afterEach(function () {
+describe('helpers::cookies', () => {
+  afterEach(() => {
     // Remove all the cookies
     var expires = Date.now() - (60 * 60 * 24 * 7);
-    document.cookie.split(';').map(function (cookie) {
-      return cookie.split('=')[0];
-    }).forEach(function (name) {
+    document.cookie.split(';').map(cookie => cookie.split('=')[0]).forEach(name => {
       document.cookie = name + '=; expires=' + new Date(expires).toGMTString();
     });
   });
 
-  it('should write cookies', function () {
+  it('should write cookies', () => {
     cookies.write('foo', 'baz');
     expect(document.cookie).toEqual('foo=baz');
   });
 
-  it('should read cookies', function () {
+  it('should read cookies', () => {
     cookies.write('foo', 'abc');
     cookies.write('bar', 'def');
     expect(cookies.read('foo')).toEqual('abc');
     expect(cookies.read('bar')).toEqual('def');
   });
 
-  it('should remove cookies', function () {
+  it('should remove cookies', () => {
     cookies.write('foo', 'bar');
     cookies.remove('foo');
     expect(cookies.read('foo')).toEqual(null);
   });
 
-  it('should uri encode values', function () {
+  it('should uri encode values', () => {
     cookies.write('foo', 'bar baz%');
     expect(document.cookie).toEqual('foo=bar%20baz%25');
   });

f6758f841e6a8a86ae7d31e83c6d2d8f24c6e744
kale-io <cos+a@iuvo.ai>
2018-07-20 10:50:30 -0700

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/specs/helpers/isURLSameOrigin.spec.js
+++ test/specs/helpers/isURLSameOrigin.spec.js
@@ -1,11 +1,11 @@
 var isURLSameOrigin = require('../../../lib/helpers/isURLSameOrigin');
 
-describe('helpers::isURLSameOrigin', function () {
-  it('should detect same origin', function () {
+describe('helpers::isURLSameOrigin', () => {
+  it('should detect same origin', () => {
     expect(isURLSameOrigin(window.location.href)).toEqual(true);
   });
 
-  it('should detect different origin', function () {
+  it('should detect different origin', () => {
     expect(isURLSameOrigin('https://github.com/axios/axios')).toEqual(false);
   });
 });

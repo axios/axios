df191e42e2dd38d91954176a3d4945dca4742ede
kale-io <cos+a@iuvo.ai>
2018-07-20 10:49:59 -0700

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/specs/headers.spec.js
+++ test/specs/headers.spec.js
@@ -18,21 +18,21 @@ function testHeaderValue(headers, key, val) {
   }
 }
 
-describe('headers', function () {
-  beforeEach(function () {
+describe('headers', () => {
+  beforeEach(() => {
     jasmine.Ajax.install();
   });
 
-  afterEach(function () {
+  afterEach(() => {
     jasmine.Ajax.uninstall();
   });
 
-  it('should default common headers', function (done) {
+  it('should default common headers', done => {
     var headers = axios.defaults.headers.common;
 
     axios('/foo');
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       for (var key in headers) {
         if (headers.hasOwnProperty(key)) {
           expect(request.requestHeaders[key]).toEqual(headers[key]);
@@ -42,12 +42,12 @@ describe('headers', function () {
     });
   });
 
-  it('should add extra headers for post', function (done) {
+  it('should add extra headers for post', done => {
     var headers = axios.defaults.headers.common;
 
     axios.post('/foo', 'fizz=buzz');
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       for (var key in headers) {
         if (headers.hasOwnProperty(key)) {
           expect(request.requestHeaders[key]).toEqual(headers[key]);
@@ -57,31 +57,31 @@ describe('headers', function () {
     });
   });
 
-  it('should use application/json when posting an object', function (done) {
+  it('should use application/json when posting an object', done => {
     axios.post('/foo/bar', {
       firstName: 'foo',
       lastName: 'bar'
     });
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       testHeaderValue(request.requestHeaders, 'Content-Type', 'application/json;charset=utf-8');
       done();
     });
   });
 
-  it('should remove content-type if data is empty', function (done) {
+  it('should remove content-type if data is empty', done => {
     axios.post('/foo');
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       testHeaderValue(request.requestHeaders, 'Content-Type', undefined);
       done();
     });
   });
 
-  it('should preserve content-type if data is false', function (done) {
+  it('should preserve content-type if data is false', done => {
     axios.post('/foo', false);
 
-    getAjaxRequest().then(function (request) {
+    getAjaxRequest().then(request => {
       testHeaderValue(request.requestHeaders, 'Content-Type', 'application/x-www-form-urlencoded');
       done();
     });

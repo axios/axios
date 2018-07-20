6b85886edeca1313d7b812a75ad72fd95b12d2d7
kale-io <cos+a@iuvo.ai>
2018-07-20 10:49:17 -0700

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/specs/cancel/Cancel.spec.js
+++ test/specs/cancel/Cancel.spec.js
@@ -1,13 +1,13 @@
 var Cancel = require('../../../lib/cancel/Cancel');
 
-describe('Cancel', function() {
-  describe('toString', function() {
-    it('returns correct result when message is not specified', function() {
+describe('Cancel', () => {
+  describe('toString', () => {
+    it('returns correct result when message is not specified', () => {
       var cancel = new Cancel();
       expect(cancel.toString()).toBe('Cancel');
     });
 
-    it('returns correct result when message is specified', function() {
+    it('returns correct result when message is specified', () => {
       var cancel = new Cancel('Operation has been canceled.');
       expect(cancel.toString()).toBe('Cancel: Operation has been canceled.');
     });

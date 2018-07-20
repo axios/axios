cdd8b63e865738f28e565212110228211fe06e5f
kale-io <cos+a@iuvo.ai>
2018-07-20 10:49:11 -0700

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/specs/cancel.spec.js
+++ test/specs/cancel.spec.js
@@ -1,22 +1,22 @@
 var Cancel = axios.Cancel;
 var CancelToken = axios.CancelToken;
 
-describe('cancel', function() {
-  beforeEach(function() {
+describe('cancel', () => {
+  beforeEach(() => {
     jasmine.Ajax.install();
   });
 
-  afterEach(function() {
+  afterEach(() => {
     jasmine.Ajax.uninstall();
   });
 
-  describe('when called before sending request', function() {
-    it('rejects Promise with a Cancel object', function (done) {
+  describe('when called before sending request', () => {
+    it('rejects Promise with a Cancel object', done => {
       var source = CancelToken.source();
       source.cancel('Operation has been canceled.');
       axios.get('/foo', {
         cancelToken: source.token
-      }).catch(function (thrown) {
+      }).catch(thrown => {
         expect(thrown).toEqual(jasmine.any(Cancel));
         expect(thrown.message).toBe('Operation has been canceled.');
         done();
@@ -24,18 +24,18 @@ describe('cancel', function() {
     });
   });
 
-  describe('when called after request has been sent', function() {
-    it('rejects Promise with a Cancel object', function (done) {
+  describe('when called after request has been sent', () => {
+    it('rejects Promise with a Cancel object', done => {
       var source = CancelToken.source();
       axios.get('/foo/bar', {
         cancelToken: source.token
-      }).catch(function (thrown) {
+      }).catch(thrown => {
         expect(thrown).toEqual(jasmine.any(Cancel));
         expect(thrown.message).toBe('Operation has been canceled.');
         done();
       });
 
-      getAjaxRequest().then(function (request) {
+      getAjaxRequest().then(request => {
         // call cancel() when the request has been sent, but a response has not been received
         source.cancel('Operation has been canceled.');
         request.respondWith({
@@ -45,18 +45,18 @@ describe('cancel', function() {
       });
     });
 
-    it('calls abort on request object', function (done) {
+    it('calls abort on request object', done => {
       var source = CancelToken.source();
       var request;
       axios.get('/foo/bar', {
         cancelToken: source.token
-      }).catch(function() {
+      }).catch(() => {
         // jasmine-ajax sets statusText to 'abort' when request.abort() is called
         expect(request.statusText).toBe('abort');
         done();
       });
 
-      getAjaxRequest().then(function (req) {
+      getAjaxRequest().then(req => {
         // call cancel() when the request has been sent, but a response has not been received
         source.cancel();
         request = req;
@@ -64,21 +64,21 @@ describe('cancel', function() {
     });
   });
 
-  describe('when called after response has been received', function() {
+  describe('when called after response has been received', () => {
     // https://github.com/axios/axios/issues/482
-    it('does not cause unhandled rejection', function (done) {
+    it('does not cause unhandled rejection', done => {
       var source = CancelToken.source();
       axios.get('/foo', {
         cancelToken: source.token
-      }).then(function () {
-        window.addEventListener('unhandledrejection', function () {
+      }).then(() => {
+        window.addEventListener('unhandledrejection', () => {
           done.fail('Unhandled rejection.');
         });
         source.cancel();
         setTimeout(done, 100);
       });
 
-      getAjaxRequest().then(function (request) {
+      getAjaxRequest().then(request => {
         request.respondWith({
           status: 200,
           responseText: 'OK'

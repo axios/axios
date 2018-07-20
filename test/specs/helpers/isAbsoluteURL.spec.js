85e1175b02656822745f151fa88362770f07f42d
kale-io <cos+a@iuvo.ai>
2018-07-20 10:50:26 -0700

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/specs/helpers/isAbsoluteURL.spec.js
+++ test/specs/helpers/isAbsoluteURL.spec.js
@@ -1,22 +1,22 @@
 var isAbsoluteURL = require('../../../lib/helpers/isAbsoluteURL');
 
-describe('helpers::isAbsoluteURL', function () {
-  it('should return true if URL begins with valid scheme name', function () {
+describe('helpers::isAbsoluteURL', () => {
+  it('should return true if URL begins with valid scheme name', () => {
     expect(isAbsoluteURL('https://api.github.com/users')).toBe(true);
     expect(isAbsoluteURL('custom-scheme-v1.0://example.com/')).toBe(true);
     expect(isAbsoluteURL('HTTP://example.com/')).toBe(true);
   });
 
-  it('should return false if URL begins with invalid scheme name', function () {
+  it('should return false if URL begins with invalid scheme name', () => {
     expect(isAbsoluteURL('123://example.com/')).toBe(false);
     expect(isAbsoluteURL('!valid://example.com/')).toBe(false);
   });
 
-  it('should return true if URL is protocol-relative', function () {
+  it('should return true if URL is protocol-relative', () => {
     expect(isAbsoluteURL('//example.com/')).toBe(true);
   });
 
-  it('should return false if URL is relative', function () {
+  it('should return false if URL is relative', () => {
     expect(isAbsoluteURL('/foo')).toBe(false);
     expect(isAbsoluteURL('foo')).toBe(false);
   });

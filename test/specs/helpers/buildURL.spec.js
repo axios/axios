b82e58fb72fbe6d9cbf0e1640256385967059afc
kale-io <cos+a@iuvo.ai>
2019-03-07 23:54:36 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/specs/helpers/buildURL.spec.js
+++ test/specs/helpers/buildURL.spec.js
@@ -1,18 +1,18 @@
 var buildURL = require('../../../lib/helpers/buildURL');
 var URLSearchParams = require('url-search-params');
 
-describe('helpers::buildURL', function () {
-  it('should support null params', function () {
+describe('helpers::buildURL', () => {
+  it('should support null params', () => {
     expect(buildURL('/foo')).toEqual('/foo');
   });
 
-  it('should support params', function () {
+  it('should support params', () => {
     expect(buildURL('/foo', {
       foo: 'bar'
     })).toEqual('/foo?foo=bar');
   });
 
-  it('should support object params', function () {
+  it('should support object params', () => {
     expect(buildURL('/foo', {
       foo: {
         bar: 'baz'
@@ -20,7 +20,7 @@ describe('helpers::buildURL', function () {
     })).toEqual('/foo?foo=' + encodeURI('{"bar":"baz"}'));
   });
 
-  it('should support date params', function () {
+  it('should support date params', () => {
     var date = new Date();
 
     expect(buildURL('/foo', {
@@ -28,25 +28,25 @@ describe('helpers::buildURL', function () {
     })).toEqual('/foo?date=' + date.toISOString());
   });
 
-  it('should support array params', function () {
+  it('should support array params', () => {
     expect(buildURL('/foo', {
       foo: ['bar', 'baz']
     })).toEqual('/foo?foo[]=bar&foo[]=baz');
   });
 
-  it('should support special char params', function () {
+  it('should support special char params', () => {
     expect(buildURL('/foo', {
       foo: '@:$, '
     })).toEqual('/foo?foo=@:$,+');
   });
 
-  it('should support existing params', function () {
+  it('should support existing params', () => {
     expect(buildURL('/foo?foo=bar', {
       bar: 'baz'
     })).toEqual('/foo?foo=bar&bar=baz');
   });
 
-  it('should support "length" parameter', function () {
+  it('should support "length" parameter', () => {
     expect(buildURL('/foo', {
       query: 'bar',
       start: 0,
@@ -54,13 +54,13 @@ describe('helpers::buildURL', function () {
     })).toEqual('/foo?query=bar&start=0&length=5');
   });
 
-  it('should correct discard url hash mark', function () {
+  it('should correct discard url hash mark', () => {
     expect(buildURL('/foo?foo=bar#hash', {
       query: 'baz'
     })).toEqual('/foo?foo=bar&query=baz');
   });
 
-  it('should use serializer if provided', function () {
+  it('should use serializer if provided', () => {
     serializer = sinon.stub();
     params = {foo: 'bar'};
     serializer.returns('foo=bar');
@@ -69,7 +69,7 @@ describe('helpers::buildURL', function () {
     expect(serializer.calledWith(params)).toBe(true);
   });
 
-  it('should support URLSearchParams', function () {
+  it('should support URLSearchParams', () => {
     expect(buildURL('/foo', new URLSearchParams('bar=baz'))).toEqual('/foo?bar=baz');
   });
 });

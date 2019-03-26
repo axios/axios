34990445c484dcc59cd8eef8e4f5b6965d73b0ea
Vlad <vldvel@rambler.ru>
2019-03-26 17:34:58 +0300

    Fix type of progressEvent
    https://github.com/Microsoft/TypeScript/blob/master/lib/lib.dom.d.ts#L11477

--- index.d.ts
+++ index.d.ts
@@ -55,8 +55,8 @@ export interface AxiosRequestConfig {
   responseType?: ResponseType;
   xsrfCookieName?: string;
   xsrfHeaderName?: string;
-  onUploadProgress?: (progressEvent: any) => void;
-  onDownloadProgress?: (progressEvent: any) => void;
+  onUploadProgress?: (progressEvent: ProgressEvent) => void;
+  onDownloadProgress?: (progressEvent: ProgressEvent) => void;
   maxContentLength?: number;
   validateStatus?: (status: number) => boolean;
   maxRedirects?: number;

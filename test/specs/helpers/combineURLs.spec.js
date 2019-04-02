5fb41bf533349a6dc5f3484fbd25c11254d13008
Sohail Ashraf <me.sohail31@gmail.com>
2019-04-02 18:19:43 +0800

    Adding test case to validate the default protocol.

--- test/specs/helpers/combineURLs.spec.js
+++ test/specs/helpers/combineURLs.spec.js
@@ -20,4 +20,8 @@ describe('helpers::combineURLs', function () {
   it('should allow a single slash for relative url', function () {
     expect(combineURLs('https://api.github.com/users', '/')).toBe('https://api.github.com/users/');
   });
+  
+  it('should return a url with default http protocol if not provided', function () {
+    expect(combineURLs('api.github.com/users')).toBe('http://api.github.com/users');
+  });
 });

55dc1cd010cf74991148673b10bedb38d340a5f7
Yasu Flores <carlosyasu91@gmail.com>
2019-11-06 00:39:26 -0800

    Fix for http2 test files

--- http2_test.js
+++ http2_test.js
@@ -4,12 +4,11 @@ var fs = require('fs');
 axios.defaults.experimental_http2 = true;
 
 axios({
-  method: 'get',
-  url: 'http://bit.ly/2mTM3nY',
-  responseType: 'stream'
+  method: 'GET',
+  url: 'https://localhost:8443/',
+  certificatePath: './localhost-cert.pem',
 }).then(function (response) {
     console.log(response);
-    response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
   }).catch(function (error) {
     console.log(error);
   })

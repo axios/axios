fd81cc8fe95c007af4c3fee1822ab017d6384376
Yasu Flores <carlosyasu91@gmail.com>
2019-11-07 18:40:58 -0800

    Update build

--- dist/axios.js
+++ dist/axios.js
@@ -1467,9 +1467,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	'use strict';
 	
 	module.exports = function isValidXss(requestURL) {
-	  var xssRegex = /(\b)(on\S+)(\s*)=|javascript|(<\s*)(\/*)script/gi;
+	  var xssRegex = /(\b)(on\w+)=|javascript|(<\s*)(\/*)script/gi;
 	  return xssRegex.test(requestURL);
 	};
+	
 
 
 /***/ }),

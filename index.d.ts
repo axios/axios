29896f60db882304ce3aaefa212101c694b4a87f
Igor Strebezhev <xamgore@users.noreply.github.com>
2019-11-15 03:01:56 +0300

    Refine arguments for error interceptors

--- index.d.ts
+++ index.d.ts
@@ -120,7 +120,7 @@ export interface CancelTokenSource {
 }
 
 export interface AxiosInterceptorManager<V> {
-  use(onFulfilled?: (value: V) => V | Promise<V>, onRejected?: (error: any) => any): number;
+  use(onFulfilled?: (value: V) => V | Promise<V>, onRejected?: (error: AxiosError<V> | any) => any): number;
   eject(id: number): void;
 }
 

72c409d347f1050ebdb34f371f474227f6c39c7c
Tomxuetao <1127513120@qq.com>
2020-01-03 14:18:53 +0800

    格式化代码

--- examples/get/server.js
+++ examples/get/server.js
@@ -1,34 +1,34 @@
 var people = [
-  {
-    "name": "Matt Zabriskie",
-    "github": "mzabriskie",
-    "twitter": "mzabriskie",
-    "avatar": "199035"
-  },
-  {
-    "name": "Ryan Florence",
-    "github": "rpflorence",
-    "twitter": "ryanflorence",
-    "avatar": "100200"
-  },
-  {
-    "name": "Kent C. Dodds",
-    "github": "kentcdodds",
-    "twitter": "kentcdodds",
-    "avatar": "1500684"
-  },
-  {
-    "name": "Chris Esplin",
-    "github": "deltaepsilon",
-    "twitter": "chrisesplin",
-    "avatar": "878947"
-  }
+    {
+        "name": "Matt Zabriskie",
+        "github": "mzabriskie",
+        "twitter": "mzabriskie",
+        "avatar": "199035"
+    },
+    {
+        "name": "Ryan Florence",
+        "github": "rpflorence",
+        "twitter": "ryanflorence",
+        "avatar": "100200"
+    },
+    {
+        "name": "Kent C. Dodds",
+        "github": "kentcdodds",
+        "twitter": "kentcdodds",
+        "avatar": "1500684"
+    },
+    {
+        "name": "Chris Esplin",
+        "github": "deltaepsilon",
+        "twitter": "chrisesplin",
+        "avatar": "878947"
+    }
 ];
 
 module.exports = function (req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/json'
-  });
-  res.write(JSON.stringify(people));
-  res.end();
+    res.writeHead(200, {
+        'Content-Type': 'text/json'
+    });
+    res.write(JSON.stringify(people));
+    res.end();
 };
--- sandbox/client.js
+++ sandbox/client.js
@@ -2,19 +2,24 @@ var axios = require('../index');
 
 var URL = 'http://127.0.0.1:3000/api';
 var BODY = {
-  foo: 'bar',
-  baz: 1234
+    foo: 'bar',
+    baz: 1234
 };
 
-function handleSuccess(data) { console.log(data); }
-function handleFailure(data) { console.log('error', data); }
+function handleSuccess (data) {
+    console.log(data);
+}
+
+function handleFailure (data) {
+    console.log('error', data);
+}
 
 // GET
 axios.get(URL, { params: BODY })
-  .then(handleSuccess)
-  .catch(handleFailure);
+    .then(handleSuccess)
+    .catch(handleFailure);
 
 // POST
 axios.post(URL, BODY)
-  .then(handleSuccess)
-  .catch(handleFailure);
+    .then(handleSuccess)
+    .catch(handleFailure);

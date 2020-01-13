d5ef300487eb9ff3a9da74685c50c8fa76ffaa6d
ak71845 <alexandre.korotkov@kroger.com>
2020-01-13 10:50:01 -0500

    no dist files commited
    - remove yarn lock because it points to our registry

--- test/specs/adapter.spec.js
+++ test/specs/adapter.spec.js
@@ -1,7 +1,9 @@
 var axios = require('../../index');
 
 describe('adapter', function () {
-  it('should support custom adapter', function (done) {
+  it('should support custom adapter (asynchronously)', function (done) {
+
+    var promiseResolveSpy = spyOn(window.Promise, 'resolve').and.callThrough();
     var called = false;
 
     axios('/foo', {
@@ -11,6 +13,7 @@ describe('adapter', function () {
     });
 
     setTimeout(function () {
+      expect(promiseResolveSpy).toHaveBeenCalled();
       expect(called).toBe(true);
       done();
     }, 100);

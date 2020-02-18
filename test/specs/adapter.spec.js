cfb8d6ee0d4767d591954eb5d2acd9fc514285e4
SashaKoro <balaskorotkov@gmail.com>
2020-02-18 16:15:36 -0500

    issue#2609 | Sasha | add 2 adapter unit tests
    - remove check for requestCancelled

--- test/specs/adapter.spec.js
+++ test/specs/adapter.spec.js
@@ -33,4 +33,66 @@ describe('adapter', function () {
       done();
     });
   });
+
+  it('should execute adapter code synchronously', function (done) {
+    var asyncFlag = false;
+    axios('/foo', {
+      adapter: function barAdapter(config) {
+        return new Promise(function dispatchXhrRequest(resolve) {
+          var request = new XMLHttpRequest();
+          request.open('GET', '/bar');
+
+          request.onreadystatechange = function () {
+            resolve({
+              config: config,
+              request: request
+            });
+          };
+
+          expect(asyncFlag).toBe(false);
+          request.send(null);
+        });
+      }
+    }).catch(console.log);
+
+    asyncFlag = true;
+
+    getAjaxRequest().then(function() {
+      done();
+    });
+  });
+
+  it('should execute adapter code asynchronously when interceptor is present', function (done) {
+    var asyncFlag = false;
+
+    axios.interceptors.request.use(function (config) {
+      config.headers.async = 'async it!';
+      return config;
+    });
+
+    axios('/foo', {
+      adapter: function barAdapter(config) {
+        return new Promise(function dispatchXhrRequest(resolve) {
+          var request = new XMLHttpRequest();
+          request.open('GET', '/bar');
+
+          request.onreadystatechange = function () {
+            resolve({
+              config: config,
+              request: request
+            });
+          };
+
+          expect(asyncFlag).toBe(true);
+          request.send(null);
+        });
+      }
+    }).catch(console.log);
+
+    asyncFlag = true;
+
+    getAjaxRequest().then(function() {
+      done();
+    });
+  });
 });

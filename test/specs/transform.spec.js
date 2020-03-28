e4d1b8ff924220debd81903d9d271bb5a5498cf0
Xianming Zhong <chinesedfan@qq.com>
2020-03-28 20:49:16 +0800

    add test

--- test/specs/transform.spec.js
+++ test/specs/transform.spec.js
@@ -41,6 +41,29 @@ describe('transform', function () {
     });
   });
 
+  it('should not transform string to JSON if responseType is text', function (done) {
+    var response;
+
+    axios('/foo', {
+      responseType: 'text'
+    }).then(function (data) {
+      response = data;
+    });
+
+    getAjaxRequest().then(function (request) {
+      request.respondWith({
+        status: 200,
+        responseText: '{"foo": "bar"}'
+      });
+
+      setTimeout(function () {
+        expect(typeof response.data).toEqual('string');
+        expect(response.data).toEqual('{"foo": "bar"}');
+        done();
+      }, 100);
+    });
+  });
+
   it('should override default transform', function (done) {
     var data = {
       foo: 'bar'

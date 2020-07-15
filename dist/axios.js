ffea03453f77a8176c51554d5f6c3c6829294649
Emily Morehouse <emily@cuttlesoft.com>
2020-07-15 10:04:59 -0600

    Releasing 0.20.0-0

--- dist/axios.js
+++ dist/axios.js
@@ -1,3 +1,4 @@
+/* axios v0.20.0-0 | (c) 2020 by Matt Zabriskie */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -227,6 +228,21 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 	
 	/**
+	 * Determine if a value is a plain Object
+	 *
+	 * @param {Object} val The value to test
+	 * @return {boolean} True if value is a plain Object, otherwise false
+	 */
+	function isPlainObject(val) {
+	  if (toString.call(val) !== '[object Object]') {
+	    return false;
+	  }
+	
+	  var prototype = Object.getPrototypeOf(val);
+	  return prototype === null || prototype === Object.prototype;
+	}
+	
+	/**
 	 * Determine if a value is a Date
 	 *
 	 * @param {Object} val The value to test
@@ -382,34 +398,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	function merge(/* obj1, obj2, obj3, ... */) {
 	  var result = {};
 	  function assignValue(val, key) {
-	    if (typeof result[key] === 'object' && typeof val === 'object') {
+	    if (isPlainObject(result[key]) && isPlainObject(val)) {
 	      result[key] = merge(result[key], val);
-	    } else {
-	      result[key] = val;
-	    }
-	  }
-	
-	  for (var i = 0, l = arguments.length; i < l; i++) {
-	    forEach(arguments[i], assignValue);
-	  }
-	  return result;
-	}
-	
-	/**
-	 * Function equal to merge with the difference being that no reference
-	 * to original objects is kept.
-	 *
-	 * @see merge
-	 * @param {Object} obj1 Object to merge
-	 * @returns {Object} Result of all merge properties
-	 */
-	function deepMerge(/* obj1, obj2, obj3, ... */) {
-	  var result = {};
-	  function assignValue(val, key) {
-	    if (typeof result[key] === 'object' && typeof val === 'object') {
-	      result[key] = deepMerge(result[key], val);
-	    } else if (typeof val === 'object') {
-	      result[key] = deepMerge({}, val);
+	    } else if (isPlainObject(val)) {
+	      result[key] = merge({}, val);
+	    } else if (isArray(val)) {
+	      result[key] = val.slice();
 	    } else {
 	      result[key] = val;
 	    }
@@ -440,6 +434,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	  return a;
 	}
 	
+	/**
+	 * Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
+	 *
+	 * @param {string} content with BOM
+	 * @return {string} content value without BOM
+	 */
+	function stripBOM(content) {
+	  if (content.charCodeAt(0) === 0xFEFF) {
+	    content = content.slice(1);
+	  }
+	  return content;
+	}
+	
 	module.exports = {
 	  isArray: isArray,
 	  isArrayBuffer: isArrayBuffer,
@@ -449,6 +456,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  isString: isString,
 	  isNumber: isNumber,
 	  isObject: isObject,
+	  isPlainObject: isPlainObject,
 	  isUndefined: isUndefined,
 	  isDate: isDate,
 	  isFile: isFile,
@@ -459,9 +467,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	  isStandardBrowserEnv: isStandardBrowserEnv,
 	  forEach: forEach,
 	  merge: merge,
-	  deepMerge: deepMerge,
 	  extend: extend,
-	  trim: trim
+	  trim: trim,
+	  stripBOM: stripBOM
 	};
 
 
@@ -561,7 +569,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	utils.forEach(['delete', 'get', 'head', 'options'], function forEachMethodNoData(method) {
 	  /*eslint func-names:0*/
 	  Axios.prototype[method] = function(url, config) {
-	    return this.request(utils.merge(config || {}, {
+	    return this.request(mergeConfig(config || {}, {
 	      method: method,
 	      url: url
 	    }));
@@ -571,7 +579,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	utils.forEach(['post', 'put', 'patch'], function forEachMethodWithData(method) {
 	  /*eslint func-names:0*/
 	  Axios.prototype[method] = function(url, data, config) {
-	    return this.request(utils.merge(config || {}, {
+	    return this.request(mergeConfig(config || {}, {
 	      method: method,
 	      url: url,
 	      data: data
@@ -592,7 +600,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	function encode(val) {
 	  return encodeURIComponent(val).
-	    replace(/%40/gi, '@').
 	    replace(/%3A/gi, ':').
 	    replace(/%24/g, '$').
 	    replace(/%2C/gi, ',').
@@ -919,6 +926,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  xsrfHeaderName: 'X-XSRF-TOKEN',
 	
 	  maxContentLength: -1,
+	  maxBodyLength: -1,
 	
 	  validateStatus: function validateStatus(status) {
 	    return status >= 200 && status < 300;
@@ -968,10 +976,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	var utils = __webpack_require__(2);
 	var settle = __webpack_require__(13);
+	var cookies = __webpack_require__(16);
 	var buildURL = __webpack_require__(5);
-	var buildFullPath = __webpack_require__(16);
-	var parseHeaders = __webpack_require__(19);
-	var isURLSameOrigin = __webpack_require__(20);
+	var buildFullPath = __webpack_require__(17);
+	var parseHeaders = __webpack_require__(20);
+	var isURLSameOrigin = __webpack_require__(21);
 	var createError = __webpack_require__(14);
 	
 	module.exports = function xhrAdapter(config) {
@@ -983,12 +992,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	      delete requestHeaders['Content-Type']; // Let the browser set it
 	    }
 	
+	    if (
+	      (utils.isBlob(requestData) || utils.isFile(requestData)) &&
+	      requestData.type
+	    ) {
+	      delete requestHeaders['Content-Type']; // Let the browser set it
+	    }
+	
 	    var request = new XMLHttpRequest();
 	
 	    // HTTP basic authentication
 	    if (config.auth) {
 	      var username = config.auth.username || '';
-	      var password = config.auth.password || '';
+	      var password = unescape(encodeURIComponent(config.auth.password)) || '';
 	      requestHeaders.Authorization = 'Basic ' + btoa(username + ':' + password);
 	    }
 	
@@ -1069,8 +1085,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	    // This is only done if running in a standard browser environment.
 	    // Specifically not if we're in a web worker, or react-native.
 	    if (utils.isStandardBrowserEnv()) {
-	      var cookies = __webpack_require__(21);
-	
 	      // Add xsrf header
 	      var xsrfValue = (config.withCredentials || isURLSameOrigin(fullPath)) && config.xsrfCookieName ?
 	        cookies.read(config.xsrfCookieName) :
@@ -1136,7 +1150,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      });
 	    }
 	
-	    if (requestData === undefined) {
+	    if (!requestData) {
 	      requestData = null;
 	    }
 	
@@ -1163,7 +1177,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	 */
 	module.exports = function settle(resolve, reject, response) {
 	  var validateStatus = response.config.validateStatus;
-	  if (!validateStatus || validateStatus(response.status)) {
+	  if (!response.status || !validateStatus || validateStatus(response.status)) {
 	    resolve(response);
 	  } else {
 	    reject(createError(
@@ -1227,7 +1241,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  error.response = response;
 	  error.isAxiosError = true;
 	
-	  error.toJSON = function() {
+	  error.toJSON = function toJSON() {
 	    return {
 	      // Standard
 	      message: this.message,
@@ -1255,8 +1269,67 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 	
-	var isAbsoluteURL = __webpack_require__(17);
-	var combineURLs = __webpack_require__(18);
+	var utils = __webpack_require__(2);
+	
+	module.exports = (
+	  utils.isStandardBrowserEnv() ?
+	
+	  // Standard browser envs support document.cookie
+	    (function standardBrowserEnv() {
+	      return {
+	        write: function write(name, value, expires, path, domain, secure) {
+	          var cookie = [];
+	          cookie.push(name + '=' + encodeURIComponent(value));
+	
+	          if (utils.isNumber(expires)) {
+	            cookie.push('expires=' + new Date(expires).toGMTString());
+	          }
+	
+	          if (utils.isString(path)) {
+	            cookie.push('path=' + path);
+	          }
+	
+	          if (utils.isString(domain)) {
+	            cookie.push('domain=' + domain);
+	          }
+	
+	          if (secure === true) {
+	            cookie.push('secure');
+	          }
+	
+	          document.cookie = cookie.join('; ');
+	        },
+	
+	        read: function read(name) {
+	          var match = document.cookie.match(new RegExp('(^|;\\s*)(' + name + ')=([^;]*)'));
+	          return (match ? decodeURIComponent(match[3]) : null);
+	        },
+	
+	        remove: function remove(name) {
+	          this.write(name, '', Date.now() - 86400000);
+	        }
+	      };
+	    })() :
+	
+	  // Non standard browser env (web workers, react-native) lack needed support.
+	    (function nonStandardBrowserEnv() {
+	      return {
+	        write: function write() {},
+	        read: function read() { return null; },
+	        remove: function remove() {}
+	      };
+	    })()
+	);
+
+
+/***/ }),
+/* 17 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var isAbsoluteURL = __webpack_require__(18);
+	var combineURLs = __webpack_require__(19);
 	
 	/**
 	 * Creates a new URL by combining the baseURL with the requestedURL,
@@ -1276,7 +1349,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ }),
-/* 17 */
+/* 18 */
 /***/ (function(module, exports) {
 
 	'use strict';
@@ -1296,7 +1369,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ }),
-/* 18 */
+/* 19 */
 /***/ (function(module, exports) {
 
 	'use strict';
@@ -1316,7 +1389,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ }),
-/* 19 */
+/* 20 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -1375,7 +1448,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ }),
-/* 20 */
+/* 21 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -1449,65 +1522,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ }),
-/* 21 */
-/***/ (function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	var utils = __webpack_require__(2);
-	
-	module.exports = (
-	  utils.isStandardBrowserEnv() ?
-	
-	  // Standard browser envs support document.cookie
-	    (function standardBrowserEnv() {
-	      return {
-	        write: function write(name, value, expires, path, domain, secure) {
-	          var cookie = [];
-	          cookie.push(name + '=' + encodeURIComponent(value));
-	
-	          if (utils.isNumber(expires)) {
-	            cookie.push('expires=' + new Date(expires).toGMTString());
-	          }
-	
-	          if (utils.isString(path)) {
-	            cookie.push('path=' + path);
-	          }
-	
-	          if (utils.isString(domain)) {
-	            cookie.push('domain=' + domain);
-	          }
-	
-	          if (secure === true) {
-	            cookie.push('secure');
-	          }
-	
-	          document.cookie = cookie.join('; ');
-	        },
-	
-	        read: function read(name) {
-	          var match = document.cookie.match(new RegExp('(^|;\\s*)(' + name + ')=([^;]*)'));
-	          return (match ? decodeURIComponent(match[3]) : null);
-	        },
-	
-	        remove: function remove(name) {
-	          this.write(name, '', Date.now() - 86400000);
-	        }
-	      };
-	    })() :
-	
-	  // Non standard browser env (web workers, react-native) lack needed support.
-	    (function nonStandardBrowserEnv() {
-	      return {
-	        write: function write() {},
-	        read: function read() { return null; },
-	        remove: function remove() {}
-	      };
-	    })()
-	);
-
-
-/***/ }),
 /* 22 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -1528,59 +1542,73 @@ return /******/ (function(modules) { // webpackBootstrap
 	  config2 = config2 || {};
 	  var config = {};
 	
-	  var valueFromConfig2Keys = ['url', 'method', 'params', 'data'];
-	  var mergeDeepPropertiesKeys = ['headers', 'auth', 'proxy'];
+	  var valueFromConfig2Keys = ['url', 'method', 'data'];
+	  var mergeDeepPropertiesKeys = ['headers', 'auth', 'proxy', 'params'];
 	  var defaultToConfig2Keys = [
-	    'baseURL', 'url', 'transformRequest', 'transformResponse', 'paramsSerializer',
-	    'timeout', 'withCredentials', 'adapter', 'responseType', 'xsrfCookieName',
-	    'xsrfHeaderName', 'onUploadProgress', 'onDownloadProgress',
-	    'maxContentLength', 'validateStatus', 'maxRedirects', 'httpAgent',
-	    'httpsAgent', 'cancelToken', 'socketPath'
+	    'baseURL', 'transformRequest', 'transformResponse', 'paramsSerializer',
+	    'timeout', 'timeoutMessage', 'withCredentials', 'adapter', 'responseType', 'xsrfCookieName',
+	    'xsrfHeaderName', 'onUploadProgress', 'onDownloadProgress', 'decompress',
+	    'maxContentLength', 'maxBodyLength', 'maxRedirects', 'transport', 'httpAgent',
+	    'httpsAgent', 'cancelToken', 'socketPath', 'responseEncoding'
 	  ];
+	  var directMergeKeys = ['validateStatus'];
+	
+	  function getMergedValue(target, source) {
+	    if (utils.isPlainObject(target) && utils.isPlainObject(source)) {
+	      return utils.merge(target, source);
+	    } else if (utils.isPlainObject(source)) {
+	      return utils.merge({}, source);
+	    } else if (utils.isArray(source)) {
+	      return source.slice();
+	    }
+	    return source;
+	  }
+	
+	  function mergeDeepProperties(prop) {
+	    if (!utils.isUndefined(config2[prop])) {
+	      config[prop] = getMergedValue(config1[prop], config2[prop]);
+	    } else if (!utils.isUndefined(config1[prop])) {
+	      config[prop] = getMergedValue(undefined, config1[prop]);
+	    }
+	  }
 	
 	  utils.forEach(valueFromConfig2Keys, function valueFromConfig2(prop) {
-	    if (typeof config2[prop] !== 'undefined') {
-	      config[prop] = config2[prop];
+	    if (!utils.isUndefined(config2[prop])) {
+	      config[prop] = getMergedValue(undefined, config2[prop]);
 	    }
 	  });
 	
-	  utils.forEach(mergeDeepPropertiesKeys, function mergeDeepProperties(prop) {
-	    if (utils.isObject(config2[prop])) {
-	      config[prop] = utils.deepMerge(config1[prop], config2[prop]);
-	    } else if (typeof config2[prop] !== 'undefined') {
-	      config[prop] = config2[prop];
-	    } else if (utils.isObject(config1[prop])) {
-	      config[prop] = utils.deepMerge(config1[prop]);
-	    } else if (typeof config1[prop] !== 'undefined') {
-	      config[prop] = config1[prop];
-	    }
-	  });
+	  utils.forEach(mergeDeepPropertiesKeys, mergeDeepProperties);
 	
 	  utils.forEach(defaultToConfig2Keys, function defaultToConfig2(prop) {
-	    if (typeof config2[prop] !== 'undefined') {
-	      config[prop] = config2[prop];
-	    } else if (typeof config1[prop] !== 'undefined') {
-	      config[prop] = config1[prop];
+	    if (!utils.isUndefined(config2[prop])) {
+	      config[prop] = getMergedValue(undefined, config2[prop]);
+	    } else if (!utils.isUndefined(config1[prop])) {
+	      config[prop] = getMergedValue(undefined, config1[prop]);
+	    }
+	  });
+	
+	  utils.forEach(directMergeKeys, function merge(prop) {
+	    if (prop in config2) {
+	      config[prop] = getMergedValue(config1[prop], config2[prop]);
+	    } else if (prop in config1) {
+	      config[prop] = getMergedValue(undefined, config1[prop]);
 	    }
 	  });
 	
 	  var axiosKeys = valueFromConfig2Keys
 	    .concat(mergeDeepPropertiesKeys)
-	    .concat(defaultToConfig2Keys);
+	    .concat(defaultToConfig2Keys)
+	    .concat(directMergeKeys);
 	
 	  var otherKeys = Object
-	    .keys(config2)
+	    .keys(config1)
+	    .concat(Object.keys(config2))
 	    .filter(function filterAxiosKeys(key) {
 	      return axiosKeys.indexOf(key) === -1;
 	    });
 	
-	  utils.forEach(otherKeys, function otherKeysDefaultToConfig2(prop) {
-	    if (typeof config2[prop] !== 'undefined') {
-	      config[prop] = config2[prop];
-	    } else if (typeof config1[prop] !== 'undefined') {
-	      config[prop] = config1[prop];
-	    }
-	  });
+	  utils.forEach(otherKeys, mergeDeepProperties);
 	
 	  return config;
 	};

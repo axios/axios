110b23d4e3a2a9e2106bc5753aa3e15467dabe88
Ayon Lee <i@hyurl.com>
2020-09-16 13:13:59 +0800

    fix the signature of config.lookup

--- index.d.ts
+++ index.d.ts
@@ -71,7 +71,7 @@ export interface AxiosRequestConfig {
   proxy?: AxiosProxyConfig | false;
   cancelToken?: CancelToken;
   decompress?: boolean;
-  lookup?: (hostname: string, options: any, callback: (err: NodeJS.ErrnoException, addr: string, family: 4 | 6) => void) => void
+  lookup?: (hostname: string, options: any, callback: (err: Error, addr: string, family: 4 | 6) => void) => void
 }
 
 export interface AxiosResponse<T = any> {

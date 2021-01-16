5f315595aee05713609d8956dbe7c1fa90dc7c27
wolframkriesing <wolfram+github@kriesing.de>
2021-01-16 21:48:27 +0100

    Indent.

--- test/specs/interceptors.spec.js
+++ test/specs/interceptors.spec.js
@@ -171,37 +171,37 @@ describe('interceptors', function () {
 
   describe('when adding multiple interceptors', function () {
     describe('when the response was fulfilled', function () {
-    it('they are all executed', function (done) {
-      var response;
+      it('they are all executed', function (done) {
+        var response;
 
-      axios.interceptors.response.use(function (data) {
-        data.data = data.data + '1';
-        return data;
-      });
-      axios.interceptors.response.use(function (data) {
-        data.data = data.data + '2';
-        return data;
-      });
-      axios.interceptors.response.use(function (data) {
-        data.data = data.data + '3';
-        return data;
-      });
-
-      axios('/foo').then(function (data) {
-        response = data;
-      });
-
-      getAjaxRequest().then(function (request) {
-        request.respondWith({
-          status: 200,
-          responseText: 'OK'
+        axios.interceptors.response.use(function (data) {
+          data.data = data.data + '1';
+          return data;
+        });
+        axios.interceptors.response.use(function (data) {
+          data.data = data.data + '2';
+          return data;
+        });
+        axios.interceptors.response.use(function (data) {
+          data.data = data.data + '3';
+          return data;
         });
 
-        setTimeout(function () {
-          expect(response.data).toBe('OK123');
-          done();
-        }, 100);
-      });
+        axios('/foo').then(function (data) {
+          response = data;
+        });
+
+        getAjaxRequest().then(function (request) {
+          request.respondWith({
+            status: 200,
+            responseText: 'OK'
+          });
+
+          setTimeout(function () {
+            expect(response.data).toBe('OK123');
+            done();
+          }, 100);
+        });
       });
     });
   });

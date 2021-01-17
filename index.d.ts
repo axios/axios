b4afe5d55c79a7f4f1a228718549aec152972dcc
Igor Strebezhev <xamgore@users.noreply.github.com>
2021-01-17 09:21:20 +0300

    Refine the return type for error interceptors
    Co-authored-by: Michael Salaverry <barakplasma@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -120,7 +120,7 @@ export interface CancelTokenSource {
 }
 
 export interface AxiosInterceptorManager<V> {
-  use(onFulfilled?: (value: V) => V | Promise<V>, onRejected?: (error: AxiosError<V> | any) => any): number;
+  use(onFulfilled?: (value: V) => V | Promise<V>, onRejected?: (error: AxiosError<V> | any) => Promise<any>): number;
   eject(id: number): void;
 }
 

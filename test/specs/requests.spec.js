91980059a22fcd5c7add26a7899faa700706bc0f
wafuwafu13 <mariobaske@i.softbank.jp>
2021-05-10 11:31:47 +0900

    Adding missing url case

--- test/specs/requests.spec.js
+++ test/specs/requests.spec.js
@@ -7,6 +7,24 @@ describe('requests', function () {
     jasmine.Ajax.uninstall();
   });
 
+  it('should throw error when missing url', function (done) {
+    expect(() => axios()).toThrowError(/url setting is not valid./);
+    done();
+
+    expect(() => axios('')).toThrowError(/url setting is not valid./);
+    done();
+
+    expect(() => axios({
+      url: undefined,
+    })).toThrowError(/url setting is not valid./);
+    done();
+
+    expect(() => axios({
+      method: 'POST',
+    })).toThrowError(/url setting is not valid./);
+    done();
+  });
+
   it('should treat single string arg as url', function (done) {
     axios('/foo');
 

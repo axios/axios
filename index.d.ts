dd8c25016e58c60e6008ac4a8a85a7f91d607a9e
Irakli Safareli <i.safareli@gmail.com>
2021-06-23 16:35:31 +0400

    use Record<string, string> for headers

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 export interface AxiosTransformer {
-  (data: any, headers?: any): any;
+  (data: any, headers?: Record<string, string>): any;
 }
 
 export interface AxiosAdapter {
@@ -53,7 +53,7 @@ export interface AxiosRequestConfig {
   baseURL?: string;
   transformRequest?: AxiosTransformer | AxiosTransformer[];
   transformResponse?: AxiosTransformer | AxiosTransformer[];
-  headers?: any;
+  headers?: Record<string, string>;
   params?: any;
   paramsSerializer?: (params: any) => string;
   data?: any;
@@ -84,7 +84,7 @@ export interface AxiosResponse<T = any>  {
   data: T;
   status: number;
   statusText: string;
-  headers: any;
+  headers: Record<string, string>;
   config: AxiosRequestConfig;
   request?: any;
 }

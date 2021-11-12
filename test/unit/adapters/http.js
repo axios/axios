b4d93bd2039ad439cb3e173c8d185262df3a4e82
DigitalBrainJS <robotshara@gmail.com>
2021-11-12 17:55:47 +0200

    Fixes #4260: fixed race condition on immediate requests cancellation

--- test/unit/adapters/http.js
+++ test/unit/adapters/http.js
@@ -1034,5 +1034,32 @@ describe('supports http with nodejs', function () {
     });
   });
 
+  it('should able to cancel multiple requests with CancelToken', function(done){
+    server = http.createServer(function (req, res) {
+      res.end('ok');
+    }).listen(4444, function () {
+      var CancelToken = axios.CancelToken;
+      var source = CancelToken.source();
+      var canceledStack = [];
+
+      var requests = [1, 2, 3, 4, 5].map(function(id){
+        return axios
+          .get('/foo/bar', { cancelToken: source.token })
+          .catch(function (e) {
+            if (!axios.isCancel(e)) {
+              throw e;
+            }
+
+            canceledStack.push(id);
+          });
+      });
+
+      source.cancel("Aborted by user");
+
+      Promise.all(requests).then(function () {
+        assert.deepStrictEqual(canceledStack.sort(), [1, 2, 3, 4, 5])
+      }).then(done, done);
+    });
+  });
 });
 

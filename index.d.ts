dc7df943d5bc8ba764c7d613f483dfc94c4d7c68
Marcos Mariani <contact@mariani.dev>
2022-02-04 07:20:02 -0300

    Adding types for AxiosInterceptionOptions

--- index.d.ts
+++ index.d.ts
@@ -179,8 +179,13 @@ export interface CancelTokenSource {
   cancel: Canceler;
 }
 
+export interface AxiosInterceptorOptions<V> {
+  runWhen?(value: V): boolean
+  synchronous?: boolean
+}
+
 export interface AxiosInterceptorManager<V> {
-  use<T = V>(onFulfilled?: (value: V) => T | Promise<T>, onRejected?: (error: any) => any): number;
+  use<T = V>(onFulfilled?: (value: V) => T | Promise<T>, onRejected?: (error: any) => any, options?: AxiosInterceptorOptions<V>): number;
   eject(id: number): void;
 }
 

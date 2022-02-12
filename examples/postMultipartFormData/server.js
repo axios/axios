9d5616edc8d4d743405f2bc30f355580fe4cbdd1
carpben <bccarp@gmail.com>
2022-02-12 21:58:53 +0200

    Fix examples/postMultipartFormData/server.js to the examples/**/server.js format, and so event end runs.

--- examples/postMultipartFormData/server.js
+++ examples/postMultipartFormData/server.js
@@ -1,10 +1,13 @@
-'use strict';
-module.exports = function(req, res) {
-  req.on('end', function() {
+module.exports = function (req, res) {
+  
+  req.on('data', function (chunk) {
+  });
+
+  req.on('end', function () {
+    console.log('POST  received');
     res.writeHead(200, {
       'Content-Type': 'text/json'
     });
-
     res.end();
   });
 };

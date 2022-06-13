78ce29c7c2eea746847f9ac516a20ad0375c89c1
okxiaoliang4 <353742991@qq.com>
2022-06-13 18:34:03 +0800

    chore: format

--- lib/core/AxiosError.ts
+++ lib/core/AxiosError.ts
@@ -123,4 +123,4 @@ export default class AxiosError<T = unknown, D = any> extends Error {
   }
 }
 
-Object.defineProperty(AxiosError.prototype, 'isAxiosError', {value: true});
+Object.defineProperty(AxiosError.prototype, 'isAxiosError', { value: true });

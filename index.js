fc48377084dce4404d5e0e1874b21a9f4629f9be
Wilhelm Adrian <adrian.wilhelm@ti8m.ch>
2022-10-07 10:08:56 +0200

    fix: Export classes from type definition (#5031)

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 import axios from './lib/axios.js';
+
+export * from './lib/axios.js';
 export default axios;
--- test/unit/regression/bugs.js
+++ test/unit/regression/bugs.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import axios from '../../../index.js';
+import axios, { AxiosError, AxiosHeaders, Axios, CanceledError } from '../../../index.js';
 
 describe('issues', function () {
   describe('4999', function () {
@@ -8,6 +8,24 @@ describe('issues', function () {
 
       assert.strictEqual(data.args.foo1, 'bar1');
       assert.strictEqual(data.args.foo2, 'bar2');
+    });
+  });
+
+  describe('5031', function () {
+    it('should export class AxiosHeaders', async function () {
+      assert.doesNotThrow(() => new AxiosHeaders(), 'class AxiosHeaders not exported');
+    });
+
+    it('should export class Axios', async function () {
+      assert.doesNotThrow(() => new Axios(), 'class Axios not exported');
+    });
+
+    it('should export class AxiosError', async function () {
+      assert.doesNotThrow(() => new AxiosError('Error'), 'class AxiosError not exported');
+    });
+
+    it('should export class CanceledError', async function () {
+      assert.doesNotThrow(() => new CanceledError('Message'), 'class CanceledError not exported');
     });
   });
 });

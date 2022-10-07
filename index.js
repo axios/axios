9fc94b7dcd30af78cd8346f8b7290ac6beb4897e
Jay <jasonsaayman@gmail.com>
2022-10-07 10:43:43 +0200

    fix: revert named exports

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-import Axios from './lib/core/Axios.js';
-import AxiosHeaders from './lib/core/AxiosHeaders.js';
-import AxiosError from './lib/core/AxiosError.js';
-import CanceledError from './lib/cancel/CanceledError.js';
 import axios from './lib/axios.js';
-
-export {
-  axios as default,
-  Axios,
-  AxiosHeaders,
-  AxiosError,
-  CanceledError,
-};
+export default axios;

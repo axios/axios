3a567f1c57ccfafaae931fb65489b97fd1f71b4d
Tanner B. Hess Webber <tb@hesswebber.com>
2022-10-15 12:37:08 -0400

    fix(helpers): identifies bug in isURLSameOrigin

--- test/specs/helpers/isURLSameOrigin.spec.js
+++ test/specs/helpers/isURLSameOrigin.spec.js
@@ -8,4 +8,17 @@ describe('helpers::isURLSameOrigin', function () {
   it('should detect different origin', function () {
     expect(isURLSameOrigin('https://github.com/axios/axios')).toEqual(false);
   });
+
+  it('should gracefully handle invalid arguments', () => {
+    const allFalse = [
+      isURLSameOrigin(undefined),
+      isURLSameOrigin(null),
+      isURLSameOrigin(false),
+      isURLSameOrigin(5),
+      isURLSameOrigin([]),
+      isURLSameOrigin({}),
+    ].forEach((val) => {
+      expect(val).toBeFalse();
+    });
+  });
 });

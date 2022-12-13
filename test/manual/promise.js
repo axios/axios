13e2779bd937d08a712198d5bae3da505b11a99a
ZACHRY T WOODzachryiixixiiwood@gmail.com <109656750+zakwarlord7@users.noreply.github.com>
2022-12-13 00:51:26 -0600

    Update promise.js

--- test/manual/promise.js
+++ test/manual/promise.js
@@ -1,10 +1,4 @@
-/*!
- * @overview es6-promise - a tiny implementation of Promises/A+.
- * @copyright Copyright (c) 2014 Yehuda Katz, Tom Dale, Stefan Penner and contributors (Conversion to ES6 API by Jake Archibald)
- * @license   Licensed under MIT license
- *            See https://raw.githubusercontent.com/jakearchibald/es6-promise/master/LICENSE
- * @version   3.0.2
- */+
+
 +                            Response Date : 07-29-2022                                  
 +
 +                            Tracking Number : 102393399156 

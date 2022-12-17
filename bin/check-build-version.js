6b1bc80e46a3bab7f0cb88ef3c4dfba5b77acb6a
DigitalBrainJS <robotshara@gmail.com>
2022-12-17 22:11:01 +0200

    chore(ci): add github actions release script;

--- bin/check-build-version.js
+++ bin/check-build-version.js
@@ -2,9 +2,8 @@ import fs from 'fs';
 import assert from 'assert';
 import axios from '../index.js';
 import axiosBuild from '../dist/node/axios.cjs';
-import inquirer from 'inquirer';
 
-const {version, name} = JSON.parse(fs.readFileSync('./package.json'));
+const {version} = JSON.parse(fs.readFileSync('./package.json'));
 
 console.log('Checking versions...\n----------------------------')
 
@@ -16,27 +15,6 @@ console.log(`----------------------------`);
 assert.strictEqual(version, axios.VERSION, `Version mismatch between package and Axios`);
 assert.strictEqual(version, axiosBuild.VERSION, `Version mismatch between package and build`);
 
-console.log('PASSED\n');
-
-const choices = [
-    `Yes, let's release Axios v${version} to npm`,
-    `No, don't do an npm release - I'll do it myself`
-  ];
-
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'release',
-      message: `You have requested an npm release for ${name.toUpperCase()} v${version}. Are you sure?`,
-      choices
-    }
-  ])
-  .then((answers) => {
-    if (choices.indexOf(answers.release)) {
-      console.warn('terminate...');
-      process.exit(1);
-    }
-  });
+console.log('✔️ PASSED\n');
 
 

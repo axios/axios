146409e5d9e602602b330a30b8aed612a62e28ab
Mustafa UZUN <mustafauzun0@gmail.com>
2023-08-03 16:16:12 +0300

    == to ===

--- dist/axios.js
+++ dist/axios.js
@@ -8,10 +8,10 @@
   function _typeof(obj) {
     "@babel/helpers - typeof";
 
-    return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) {
+    return _typeof = "function" === typeof Symbol && "symbol" === typeof Symbol.iterator ? function (obj) {
       return typeof obj;
     } : function (obj) {
-      return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+      return obj && "function" === typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
     }, _typeof(obj);
   }
   function _classCallCheck(instance, Constructor) {

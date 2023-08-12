ea77e5deb894ea4cf3127fb0f6e3a91e351f1c35
Jaaatin Chhettry <jatinchhettry@gmail.com>
2023-08-12 11:18:00 +0530

    Something

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import axios from './lib/axios.js';
 
+//something
+
 // This module is intended to unwrap Axios default export as named.
 // Keep top-level export same with static properties
 // so that it can keep same with es module or cjs

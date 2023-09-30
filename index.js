1bb2be08e154561a2f2b8b66c72d80d696b016e2
Mitali Raj <129144413+Hacxmr@users.noreply.github.com>
2023-09-30 23:46:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import axios from './lib/axios.js';
 
 // This module is intended to unwrap Axios default export as named.
-// Keep top-level export same with static properties
-// so that it can keep same with es module or cjs
+// Keep top-level export the same with static properties
+// so that it can remain the same with the es module or cjs
 const {
   Axios,
   AxiosError,

1d569251eabc3bc5b60dcb4eaec43acc1f4669a7
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2023-10-19 00:00:36 +0900

    chore: add eslint globals and parserOptions

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,22 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
     js.configs.recommended,
     {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.es2018,
+                ...globals.node,
+            },
+            parserOptions: {
+                ecmaVersion: 2018,
+                sourceType: "module",
+            },
+        },
         rules: {
-            "no-cond-assign": 0
+            "no-cond-assign": 0,
         },
     }
 ];

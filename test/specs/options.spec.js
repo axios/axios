e9ac9ec0b343984b5ffa59596e6eb74bd28a3c0a
Mike Toscano <michael.andrew.toscano@gmail.com>
2024-01-18 14:21:13 -0500

    feat: update tests supporting issue #5902 functionality

--- test/specs/options.spec.js
+++ test/specs/options.spec.js
@@ -71,9 +71,23 @@ describe('options', function () {
     });
   });
 
-  it('should combine base URL with request URL if request URL is absolute', function (done) {
+  it('should ignore base URL if request URL is absolute', function (done) {
     const instance = axios.create({
       baseURL: 'http://someurl.com/'
+    });
+
+    instance.get('http://someotherurl.com/');
+
+    getAjaxRequest().then(function (request) {
+      expect(request.url).toBe('http://someotherurl.com/');
+      done();
+    });
+  });
+
+  it('should combine the URLs if base url and request url exist and allowAbsoluteUrls is false', function (done) {
+    const instance = axios.create({
+      baseURL: 'http://someurl.com/',
+      allowAbsoluteUrls: false
     });
 
     instance.get('http://someotherurl.com/');

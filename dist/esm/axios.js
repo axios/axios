0da67547f790eb16a3f43df2626e2e93f681ca03
DigitalBrainJS <DigitalBrainJS@users.noreply.github.com>
2024-01-24 23:00:14 +0000

    chore(release): v1.6.6

--- dist/esm/axios.js
+++ dist/esm/axios.js
@@ -1,4 +1,4 @@
-// Axios v1.6.5 Copyright (c) 2024 Matt Zabriskie and contributors
+// Axios v1.6.6 Copyright (c) 2024 Matt Zabriskie and contributors
 function bind(fn, thisArg) {
   return function wrap() {
     return fn.apply(thisArg, arguments);
@@ -2656,7 +2656,7 @@ function mergeConfig$1(config1, config2) {
   return config;
 }
 
-const VERSION$1 = "1.6.5";
+const VERSION$1 = "1.6.6";
 
 const validators$1 = {};
 
@@ -2771,7 +2771,28 @@ class Axios$1 {
    *
    * @returns {Promise} The Promise to be fulfilled
    */
-  request(configOrUrl, config) {
+  async request(configOrUrl, config) {
+    try {
+      return await this._request(configOrUrl, config);
+    } catch (err) {
+      const dummy = {};
+      if (Error.captureStackTrace) {
+        Error.captureStackTrace(dummy);
+      } else {
+        dummy.stack = new Error().stack;
+      }
+      // slice off the Error: ... line
+      dummy.stack = dummy.stack.replace(/^.+\n/, '');
+      // match without the 2 top stack lines
+      if (!err.stack.endsWith(dummy.stack.replace(/^.+\n.+\n/, ''))) {
+        err.stack += '\n' + dummy.stack;
+      }
+
+      throw err;
+    }
+  }
+
+  _request(configOrUrl, config) {
     /*eslint no-param-reassign:0*/
     // Allow for axios('example/url'[, config]) a la fetch API
     if (typeof configOrUrl === 'string') {

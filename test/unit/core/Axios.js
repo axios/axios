35478f210cb2a1ef46685b656eb33180bd1b3885
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2024-05-06 12:00:54 +0200

    chore: also handle undefined and unwritable stack property

--- test/unit/core/Axios.js
+++ test/unit/core/Axios.js
@@ -1,20 +1,29 @@
 import Axios from "../../../lib/core/Axios.js";
 import assert from "assert";
 
-
 describe('Axios', function () {
-    it('should support errors with un-writable stack', async function () {
-        const axios = new Axios({});
-        // mock axios._request to return an Error with an un-writable stack property
-        axios._request = () => {
-            const mockError = new Error("test-error");
-            Object.defineProperty(mockError, "stack", {value: {}, writable: false});
-            throw mockError;
-        }
-        try {
-            await axios.request("test-url", {})
-        } catch (e) {
-            assert.strictEqual(e.message, "test-error")
-        }
-    })
+  describe("handle unwritable error stack", function () {
+    async function testUnwritableErrorStack(initialStackValue) {
+      const axios = new Axios({});
+      // mock axios._request to return an Error with an un-writable stack property
+      axios._request = () => {
+        const mockError = new Error("test-error");
+        Object.defineProperty(mockError, "stack", {value: initialStackValue, writable: false});
+        throw mockError;
+      }
+      try {
+        await axios.request("test-url", {})
+      } catch (e) {
+        assert.strictEqual(e.message, "test-error")
+      }
+    }
+
+    it('should support errors with a defined but un-writable stack', async function () {
+      await testUnwritableErrorStack({})
+    });
+
+    it('should support errors with an undefined and un-writable stack', async function () {
+      await testUnwritableErrorStack(undefined)
+    });
+  })
 });

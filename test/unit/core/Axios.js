473424b68225e8980da31a331c33c57d6f9d1a26
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2024-05-06 14:36:37 +0200

    test: add getter/setter test

--- test/unit/core/Axios.js
+++ test/unit/core/Axios.js
@@ -2,13 +2,13 @@ import Axios from "../../../lib/core/Axios.js";
 import assert from "assert";
 
 describe('Axios', function () {
-  describe("handle unwritable error stack", function () {
-    async function testUnwritableErrorStack(initialStackValue) {
+  describe("handle un-writable error stack", function () {
+    async function testUnwritableErrorStack(stackAttributes) {
       const axios = new Axios({});
       // mock axios._request to return an Error with an un-writable stack property
       axios._request = () => {
         const mockError = new Error("test-error");
-        Object.defineProperty(mockError, "stack", {value: initialStackValue, writable: false});
+        Object.defineProperty(mockError, "stack", stackAttributes);
         throw mockError;
       }
       try {
@@ -19,11 +19,29 @@ describe('Axios', function () {
     }
 
     it('should support errors with a defined but un-writable stack', async function () {
-      await testUnwritableErrorStack({})
+      await testUnwritableErrorStack({value: {}, writable: false})
     });
 
     it('should support errors with an undefined and un-writable stack', async function () {
-      await testUnwritableErrorStack(undefined)
+      await testUnwritableErrorStack({value: undefined, writable: false})
+    });
+
+    it('should support errors with a custom getter/setter for the stack property', async function () {
+      await testUnwritableErrorStack({
+        get: () => ({}),
+        set: () => {
+          throw new Error('read-only');
+        }
+      })
+    });
+
+    it('should support errors with a custom getter/setter for the stack property (null case)', async function () {
+      await testUnwritableErrorStack({
+        get: () => null,
+        set: () => {
+          throw new Error('read-only');
+        }
+      })
     });
   })
 });

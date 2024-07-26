1f520927b7180db8ad4d931ed4f70006e6a73f2a
Hasnen-ali <hasnen7354@gmail.com>
2024-07-27 01:37:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import axios from './lib/axios.js';
 // This module is intended to unwrap Axios default export as named.
 // Keep top-level export same with static properties
 // so that it can keep same with es module or cjs
+// hasnen ali
 const {
   Axios,
   AxiosError,

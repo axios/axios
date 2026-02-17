b4cbf33bb3dd734e602ada5ad4e582186ea60434
Thomas Angermeier <thomas.angermeier@jambit.com>
2026-02-17 13:25:44 +0100

    fix(types): align runWhen types

--- index.d.ts
+++ index.d.ts
@@ -659,7 +659,7 @@ interface AxiosInterceptorHandler<T> {
   fulfilled: AxiosInterceptorFulfilled<T>;
   rejected?: AxiosInterceptorRejected;
   synchronous: boolean;
-  runWhen: (config: AxiosRequestConfig) => boolean | null;
+  runWhen?: ((config: AxiosRequestConfig) => boolean) | null;
 }
 
 export interface AxiosInterceptorManager<V> {

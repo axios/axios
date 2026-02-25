666c9f2172d6653efdb6aea873ae22d97400966b
janaka66 <janakaravindra74@gmail.com>
2026-02-25 14:05:38 +0530

    test: add reproduction case for missing AbortSignal reason

---
+++ test/unit/abort-reason.spec.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import axios from '../../lib/axios.js';
+import assert from 'assert';
+
+describe('AbortController abort reason', function () {
+  it('should propagate abort reason to CanceledError', async function () {
+    const controller = new AbortController();
+    setTimeout(() => controller.abort('TimeoutError'), 10);
+
+    try {
+      await axios.get('http://10.255.255.1', { signal: controller.signal });
+      assert.fail('Request should have been aborted');
+    } catch (e) {
+      assert.strictEqual(e.code, 'ERR_CANCELED');
+      assert.strictEqual(e.message, 'TimeoutError');
+    }
+  });
+});

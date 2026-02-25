66593e42f80b4f1c2193c62683cc427102fd2c67
janaka66 <janakaravindra74@gmail.com>
2026-02-25 15:18:39 +0530

    test: improve stability by increasing abort timeout

--- test/unit/abort-reason.spec.js
+++ test/unit/abort-reason.spec.js
@@ -4,7 +4,7 @@ import assert from 'assert';
 describe('AbortController abort reason', function () {
   it('should propagate abort reason to CanceledError', async function () {
     const controller = new AbortController();
-    setTimeout(() => controller.abort('TimeoutError'), 10);
+    setTimeout(() => controller.abort('TimeoutError'), 100);
 
     try {
       await axios.get('http://10.255.255.1', { signal: controller.signal });

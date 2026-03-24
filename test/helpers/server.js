2784ca87f6bb6df1bf7348c9c4875cef7b2a8640
Jay <jasonsaayman@gmail.com>
2026-03-24 20:50:56 +0200

    chore: bump selsigned to latest

--- test/helpers/server.js
+++ test/helpers/server.js
@@ -12,9 +12,11 @@ export const SERVER_HANDLER_STREAM_ECHO = (req, res) => req.pipe(res);
 
 export const setTimeoutAsync = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const certificate = selfsigned.generate(null, { keySize: 2048 });
+const certificatePromise = selfsigned.generate(null, { keySize: 2048 });
 
-export const startHTTPServer = (handlerOrOptions, options) => {
+export const startHTTPServer = async (handlerOrOptions, options) => {
+  const certificate = await certificatePromise;
+
   const {
     handler,
     useBuffering = false,

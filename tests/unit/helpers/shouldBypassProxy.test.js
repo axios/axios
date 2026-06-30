4bcacbd5f7e4c34b7afc704c0829eef1f3853b97
Andrew <112019350+andrewkernel@users.noreply.github.com>
2026-06-30 02:03:14 -0500

    fix: honor wildcard entries in NO_PROXY lists

--- tests/unit/helpers/shouldBypassProxy.test.js
+++ tests/unit/helpers/shouldBypassProxy.test.js
@@ -142,6 +142,13 @@ describe('helpers::shouldBypassProxy', () => {
     expect(shouldBypassProxy('http://[::1]:8080/')).toBe(true);
   });
 
+  it('should bypass proxy for any host when no_proxy list contains *', () => {
+    setNoProxy('localhost,*,.example.org');
+
+    expect(shouldBypassProxy('http://example.com/')).toBe(true);
+    expect(shouldBypassProxy('http://[2001:db8::1]:8080/')).toBe(true);
+  });
+
   it('should support bracketed ipv6 with explicit port in no_proxy', () => {
     setNoProxy('[::1]:8080');
 

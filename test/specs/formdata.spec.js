a930f390d8ecd262bb91c25bf22840f6ed6ec204
Shaan Majid <70789625+shaanmajid@users.noreply.github.com>
2026-07-19 08:22:26 -0500

    ci: use bundled npm for v0 publish (#11084)
    Co-authored-by: Jason Saayman <jasonsaayman@gmail.com>

--- test/specs/formdata.spec.js
+++ test/specs/formdata.spec.js
@@ -1,14 +1,30 @@
 
 describe('FormData', function() {
+  beforeEach(function () {
+    jasmine.Ajax.install();
+  });
+
+  afterEach(function () {
+    jasmine.Ajax.uninstall();
+  });
+
   it('should allow FormData posting', function () {
-    return axios.postForm('http://httpbin.org/post', {
+    var response = axios.postForm('/foo', {
       a: 'foo',
       b: 'bar'
-    }).then(({data}) => {
-      expect(data.form).toEqual({
-        a: 'foo',
-        b: 'bar'
+    });
+
+    return getAjaxRequest().then(function (request) {
+      expect(request.params).toEqual(jasmine.any(FormData));
+      expect(request.params.get('a')).toEqual('foo');
+      expect(request.params.get('b')).toEqual('bar');
+
+      request.respondWith({
+        status: 200,
+        responseText: '{}'
       });
+
+      return response;
     });
   });
-})
+});
